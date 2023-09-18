93e6921f6bbcee69d395d96d48bc548d4c9bd5ab
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-18 22:55:09 +0200

    fix(mobile): Remove react-native app (#6)
    will setup Flutter later

--- frontend/mobile/metro.config.ts
+++
@@ -1,9 +0,0 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from "expo/metro-config";
-
-const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
-});
-
-module.exports = config;
