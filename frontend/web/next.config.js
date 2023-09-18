69035412c6939b1412db7bba7c0cd011e9369f28
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-18 15:48:06 +0200

    feat(web): Docker for Next

--- frontend/web/next.config.js
+++ frontend/web/next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+	output: "standalone",
+};
 
 module.exports = nextConfig;
