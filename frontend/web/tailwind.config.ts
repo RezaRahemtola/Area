0e972af3ed26b885d5adc13c77f1eb1650e27602
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-19 13:35:32 +0200

    feat(web): DaisyUI setup

--- frontend/web/tailwind.config.ts
+++ frontend/web/tailwind.config.ts
@@ -1,19 +1,12 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./layouts/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
-		},
-	},
-	plugins: [],
+	plugins: [daisyui],
 };
 export default config;
