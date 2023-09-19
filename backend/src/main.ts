6de269c5209ec2a6b9a603d03593d43df703b72d
EdenComp <florian@cryptio.co>
2023-09-19 21:12:19 +0200

    fix: removed yarn workspaces

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './services/app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { version } from 'package.json';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./services/app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { version } from "package.json";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('AREA API')
-    .setDescription('AREA Internal API')
-    .setVersion(version)
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
+	const app = await NestFactory.create(AppModule);
+	const config = new DocumentBuilder()
+		.setTitle("AREA API")
+		.setDescription("AREA Internal API")
+		.setVersion(version)
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+	SwaggerModule.setup("api", app, document);
 
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+	app.enableCors();
+	app.useGlobalPipes(new ValidationPipe({ transform: true }));
+	await app.listen(3000);
 }
 bootstrap();
--- backend/src/services/app.module.ts
+++ backend/src/services/app.module.ts
@@ -1,29 +1,29 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env'],
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_DATABASE'),
-        entities: [],
-        migrations: [],
-        synchronize: config.get<string>('NODE_ENV') === 'development',
-      }),
-      inject: [ConfigService],
-    }),
-  ],
-  controllers: [],
-  providers: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: [".env"],
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (config: ConfigService) => ({
+				type: "postgres",
+				host: config.get<string>("DB_HOST"),
+				port: config.get<number>("DB_PORT"),
+				username: config.get<string>("DB_USERNAME"),
+				password: config.get<string>("DB_PASSWORD"),
+				database: config.get<string>("DB_DATABASE"),
+				entities: [],
+				migrations: [],
+				synchronize: config.get<string>("NODE_ENV") === "development",
+			}),
+			inject: [ConfigService],
+		}),
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
