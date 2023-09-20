a22cc18e82e152d0f711fa33ace4eb751e2499da
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-20 23:24:32 +0200

    feat(web): Landing page footer (#14)

--- frontend/web/app/(landing)/layout.tsx
+++ frontend/web/app/(landing)/layout.tsx
@@ -1,13 +1,15 @@
 import { ReactNode } from "react";
 
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 function LandingLayout({ children }: { children: ReactNode }) {
 	return (
-		<>
+		<div className="flex flex-col h-screen justify-between">
 			<Navbar />
 			{children}
-		</>
+			<Footer />
+		</div>
 	);
 }
 
