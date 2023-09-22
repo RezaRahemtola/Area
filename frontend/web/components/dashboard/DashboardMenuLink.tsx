22e8a994bda68c387a951b56677398566ad066c5
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-09-22 15:09:46 +0200

    feat(web/dashboard): create component DashboardMenuLink

---
+++ frontend/web/components/dashboard/DashboardMenuLink.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const DashboardMenuLink = ({ svgLink, title, href, isFull}: { svgLink: string, title: string, href: string, isFull: boolean }) => {
+    return (
+        <a className={isFull ? "" : "tooltip tooltip-right"} data-tip={title} href={href}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d={svgLink}
+                />
+            </svg>
+            {isFull ? title : ""}
+        </a>
+    )
+
+}
+
+export default DashboardMenuLink
