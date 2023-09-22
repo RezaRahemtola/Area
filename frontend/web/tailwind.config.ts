264b205376e69db387cf44d1e1d50d3c4647ac7d
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-09-22 13:58:31 +0200

    fix: all light theme mod with good colors

--- frontend/web/tailwind.config.ts
+++ frontend/web/tailwind.config.ts
@@ -13,17 +13,12 @@ const config: Config = {
 			{
 				light: {
 					"color-scheme": "light",
-					primary: "#570df8",
-					"primary-content": "#E0D2FE",
-					secondary: "#f000b8",
-					"secondary-content": "#FFD1F4",
-					accent: "#1ECEBC",
-					"accent-content": "#07312D",
-					neutral: "#2B3440",
-					"neutral-content": "#D7DDE4",
+					primary: "#C5C6C6",
+					secondary: "#F1895C",
+					accent: "#ffffff",
+					neutral: "#516079",
+
 					"base-100": "#ffffff",
-					"base-200": "#F2F2F2",
-					"base-300": "#E5E6E6",
 					"base-content": "#1f2937",
 				},
 				dark: {
@@ -33,8 +28,8 @@ const config: Config = {
 					accent: "#516079",
 					neutral: "#C5C6C6",
 
-					"base-100": "#000",
-					"base-content": "#FFF",
+					"base-100": "#2E3244",
+					"base-content": "#ffffff",
 				},
 			},
 		],
