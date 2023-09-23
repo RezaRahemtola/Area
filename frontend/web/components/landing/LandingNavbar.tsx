325510b040d261c34a7f057e47f78f22b1ec4d8d
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-24 00:50:39 +0200

    refactor(web/auth): General navbar

---
+++ frontend/web/components/landing/LandingNavbar.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar";
+
+const LandingNavbar = () => (
+		<Navbar
+			centerContent={
+				<ul className="menu menu-horizontal px-1">
+					<li>
+						<a>Features</a>
+					</li>
+					<li>
+						<a>Pricing</a>
+					</li>
+					<li>
+						<a>Documentation</a>
+					</li>
+					<li>
+						<a>Team</a>
+					</li>
+				</ul>
+			}
+		/>
+	);
+
+export default LandingNavbar;
