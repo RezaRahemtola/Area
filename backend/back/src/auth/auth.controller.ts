934b47e0cf20440fdfda93931d9b0a397ae16c23
Croos3r <dorian.moy@epitech.eu>
2023-09-24 23:41:11 +0200

    docs(auth-service): better description for login dtos
    Took 4 minutes

--- backend/back/src/auth/auth.controller.ts
+++ backend/back/src/auth/auth.controller.ts
@@ -1,18 +1,18 @@
 import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import LoginCredentialsDto from "./dto/login-credentials.dto";
+import CredentialsDto from "./dto/credentials.dto";
 
 @Controller("auth")
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post("login")
-	async logIn({ mail, password }: LoginCredentialsDto) {
+	async logIn({ mail, password }: CredentialsDto) {
 		return this.authService.logIn(mail, password);
 	}
 
 	@Post("register")
-	async register({ mail, password }: LoginCredentialsDto) {
+	async register({ mail, password }: CredentialsDto) {
 		return this.authService.register(mail, password);
 	}
 }
--- backend/back/src/auth/dto/login-result.dto.ts
+++ backend/back/src/auth/dto/login-result.dto.ts
@@ -2,7 +2,9 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsJWT } from "class-validator";
 
 export default class LoginResultDto {
-	@ApiProperty({ description: "The JWT access token of the user, available for 1 day" })
+	@ApiProperty({
+		description: "The JWT access token of the user, available for 1 day",
+	})
 	@IsJWT()
 	accessToken!: string;
 }
