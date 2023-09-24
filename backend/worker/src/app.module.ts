fd7f63b29deab8157f8de43c85b7b53d66cc4fb2
Croos3r <dorian.moy@epitech.eu>
2023-09-25 00:05:28 +0200

    fix: removed workers api as it is not needed
    Took 3 minutes

--- backend/worker/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { GrpcModule } from "./grpc/grpc.module";
-
-@Module({
-	imports: [GrpcModule],
-	controllers: [],
-	providers: [],
-})
-export class AppModule {}
