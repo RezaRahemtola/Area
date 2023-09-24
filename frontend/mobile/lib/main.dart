daf3461d09b8cd9a175708109c3937b45a5dc033
Axel DENIS <axel.denis@epitech.eu>
2023-09-24 15:47:30 +0200

    feat(Mobile): Finished the login page and put it in it's own file

--- frontend/mobile/lib/main.dart
+++ frontend/mobile/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AREA',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,77 +21,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Login extends StatefulWidget {
-  const Login({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.email});
 
-  final String title;
-
-  @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  final String email;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
+          title: const Text('Home Page'),
         ),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(25),
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Email",
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email.';
-                    }
-                    return null;
-                  },
-                ),
+        body: Column(
+          children: [
+            Text(email),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Go back!"),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
-                child: TextFormField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Password",
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password.';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      print("login logic here");
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                            content: Text("Please fill the login form.")),
-                      );
-                    }
-                  },
-                  child: const Text("Submit"))
-            ],
-          ),
+            ),
+          ],
         ));
   }
 }
