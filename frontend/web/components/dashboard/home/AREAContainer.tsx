139603f243ef539a1c6f6bbe03230850b6fdfef1
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-09-24 17:04:30 +0200

    feat(web/dashboard): define a minimal size for AREASContainers

--- frontend/web/components/dashboard/home/AREAContainer.tsx
+++ frontend/web/components/dashboard/home/AREAContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AREAContainer = () => (
-    <div className="w-1/3 h-1/3 bg-black"></div>
+    <div className="w-1/3 h-1/3 min-w-[188px] bg-black"></div>
 )
 
 export default AREAContainer;
