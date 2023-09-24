7c21238dec725527c2086cf31448ee71925c568a
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-09-24 17:39:56 +0200

    feat(web/dashboard): set container selection with usestate

--- frontend/web/app/dashboard/page.tsx
+++ frontend/web/app/dashboard/page.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+'use client';
+
+import React, {useState} from "react";
 import AREAContainer from "@/components/dashboard/home/AREAContainer";
 
-const DashboardPage = () => (
-    <div className="flex bg-neutral w-screen h-screen justify-center items-center flex-row">
-        <AREAContainer/>
-        <span>{"->"}</span>
-        <AREAContainer/>
-    </div>
-)
+const DashboardPage = () => {
+    const [areaSelected, setAreaSelected] = useState(1);
+
+    return (
+        <div className="flex bg-neutral w-screen h-screen justify-center items-center flex-row">
+            <AREAContainer selected={areaSelected} setSelected={setAreaSelected} id={1}/>
+            <span>{"->"}</span>
+            <AREAContainer selected={areaSelected} setSelected={setAreaSelected} id={2}/>
+            <span>{"->"}</span>
+        </div>
+    );
+}
 
 export default DashboardPage;
+
+
--- frontend/web/components/dashboard/home/AREAContainer.tsx
+++ frontend/web/components/dashboard/home/AREAContainer.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const AREAContainer = () => (
-    <div className="w-1/3 h-1/3 min-w-[188px] bg-black"></div>
-)
+const AREAContainer = ({selected, setSelected, id}: { selected: number, setSelected: (selected: number) => void, id: number }) => (
+        <div
+            className={`w-1/3 h-1/3 min-w-[188px] ${selected === id ? 'bg-accent' : 'bg-primary'} flex flex-col justify-center items-center`}>
+            {selected === id ?
+                <button className="btn btn-secondary" onClick={() => {
+                    selected === 1 ? setSelected(2) : setSelected(1);
+                }}>
+                    Create new AREA
+                </button> :
+                <div/>}
+        </div>
+    )
+
 
 export default AREAContainer;
