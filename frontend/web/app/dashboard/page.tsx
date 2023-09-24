6c10dc05169e8b4f0828e0c8983f11f8a0db5110
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-09-24 16:48:01 +0200

    feat(web/dashboard): setup base for AREA placement

--- frontend/web/app/dashboard/page.tsx
+++ frontend/web/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import AREAContainer from "@/components/dashboard/home/actionReactionContainer";
+
 const DashboardPage = () => (
-        <div className="flex bg-neutral w-screen h-screen justify-center items-center">
-            <span>Dashboard Home</span>
-        </div>
-    )
+    <div className="flex bg-neutral w-screen h-screen justify-center items-center flex-row">
+        <AREAContainer/>
+        <span>{"->"}</span>
+        <AREAContainer/>
+    </div>
+)
 
 export default DashboardPage;
---
+++ frontend/web/components/dashboard/home/actionReactionContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AREAContainer = () => (
+    <div className="w-1/3 h-1/3 bg-black"></div>
+)
+
+export default AREAContainer;
