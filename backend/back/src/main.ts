ea406a817fa8c2bff90e850d9eea784d7e911a78
Croos3r <dorian.moy@epitech.eu>
2023-09-24 18:56:20 +0200

    fix(documentation): moved documentation to a proper url "/documentation" instead of "/api"
    Took 39 seconds

--- backend/back/src/main.ts
+++ backend/back/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
 
-	SwaggerModule.setup("api", app, document);
+	SwaggerModule.setup("documentation", app, document);
 
 	app.enableCors();
 	app.useGlobalPipes(
