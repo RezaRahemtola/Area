db9516058e57fd96e7a5cc6f5ed471fc24f5944f
Croos3r <dorian.moy@epitech.eu>
2023-09-24 23:37:20 +0200

    test(user-service): added more nested describing in the user service tests
    Took 1 hour 31 minutes

--- backend/back/src/user/user.service.spec.ts
+++ backend/back/src/user/user.service.spec.ts
@@ -34,79 +34,89 @@ describe("UserService", () => {
 		createdAt: new Date("2001-09-11T00:00:00.000Z"),
 	};
 
-	it("should find an user", async () => {
-		repositoryMock.findOne.mockReturnValue(Promise.resolve(user));
-		expect(await userService.getUser({ id: user.id })).toEqual(user);
-		expect(repositoryMock.findOne).toHaveBeenCalledWith({ where: { id: user.id } });
-	});
+	describe("getUser", () => {
+		it("should find an user", async () => {
+			repositoryMock.findOne.mockReturnValue(Promise.resolve(user));
+			await expect(userService.getUser({ id: user.id })).resolves.toEqual<User | null>(user);
+			expect(repositoryMock.findOne).toHaveBeenCalledWith({ where: { id: user.id } });
+		});
 
-	it("should find no user", async () => {
-		repositoryMock.findOne.mockReturnValue(Promise.resolve(null));
-		expect(await userService.getUser({ id: user.id })).toEqual(null);
-		expect(repositoryMock.findOne).toHaveBeenCalledWith({ where: { id: user.id } });
-	});
-
-	it("should create an user and return true", async () => {
-		repositoryMock.insert.mockReturnValue(Promise.resolve({ identifiers: [{ id: user.id }] }));
-		expect(await userService.createUser(user.mail, user.passwordHash, user.isAdmin)).toEqual(true);
-		expect(repositoryMock.insert).toHaveBeenCalledWith({
-			mail: user.mail,
-			passwordHash: user.passwordHash,
-			isAdmin: user.isAdmin,
+		it("should find no user", async () => {
+			repositoryMock.findOne.mockReturnValue(Promise.resolve(null));
+			await expect(userService.getUser({ id: user.id })).resolves.toBeNull();
+			expect(repositoryMock.findOne).toHaveBeenCalledWith({ where: { id: user.id } });
 		});
 	});
 
-	it("should not create an user and return false because the user's mail is already used", async () => {
-		repositoryMock.insert.mockReturnValue(
-			Promise.reject(new Error("duplicated mail value violates unique constraint")),
-		);
-		expect(await userService.createUser("other.mail@cramptarea.org", user.passwordHash, user.isAdmin)).toEqual(false);
-		expect(repositoryMock.insert).toHaveBeenCalledWith({
-			mail: "other.mail@cramptarea.org",
-			passwordHash: user.passwordHash,
-			isAdmin: user.isAdmin,
+	describe("createUser", () => {
+		it("should create an user and return true", async () => {
+			repositoryMock.insert.mockReturnValue(Promise.resolve({ identifiers: [{ id: user.id }] }));
+			await expect(userService.createUser(user.mail, user.passwordHash, user.isAdmin)).resolves.toBeTruthy();
+			expect(repositoryMock.insert).toHaveBeenCalledWith({
+				mail: user.mail,
+				passwordHash: user.passwordHash,
+				isAdmin: user.isAdmin,
+			});
+		});
+
+		it("should not create an user and return false because the user's mail is already used", async () => {
+			repositoryMock.insert.mockReturnValue(
+				Promise.reject(new Error("duplicated mail value violates unique constraint")),
+			);
+			await expect(
+				userService.createUser("other.mail@cramptarea.org", user.passwordHash, user.isAdmin),
+			).resolves.toBeFalsy();
+			expect(repositoryMock.insert).toHaveBeenCalledWith({
+				mail: "other.mail@cramptarea.org",
+				passwordHash: user.passwordHash,
+				isAdmin: user.isAdmin,
+			});
 		});
 	});
 
-	it("should update a user according to the parameters passed and return true", async () => {
-		repositoryMock.exist.mockReturnValue(Promise.resolve(true));
-		repositoryMock.update.mockReturnValue(Promise.resolve({ affected: 1 }));
-		expect(await userService.updateUser({ id: user.id }, { isAdmin: true })).toEqual(true);
-		expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
-		expect(repositoryMock.update).toHaveBeenCalledWith({ id: user.id }, { isAdmin: true });
+	describe("updateUser", () => {
+		it("should update a user according to the parameters passed and return true", async () => {
+			repositoryMock.exist.mockReturnValue(Promise.resolve(true));
+			repositoryMock.update.mockReturnValue(Promise.resolve({ affected: 1 }));
+			expect(await userService.updateUser({ id: user.id }, { isAdmin: true })).toEqual(true);
+			expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
+			expect(repositoryMock.update).toHaveBeenCalledWith({ id: user.id }, { isAdmin: true });
+		});
+
+		it("should not update an user and return false because the new user's mail is already used by another user", async () => {
+			repositoryMock.exist.mockReturnValue(Promise.resolve(true));
+			repositoryMock.update.mockReturnValue(
+				Promise.reject(new Error("duplicated mail value violates unique constraint")),
+			);
+			expect(await userService.updateUser({ id: user.id }, { mail: "other.mail@cramptarea.org" })).toEqual(false);
+			expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
+			expect(repositoryMock.update).toHaveBeenCalledWith({ id: user.id }, { mail: "other.mail@cramptarea.org" });
+		});
+
+		it("should not update an user and return false because the user does not exists", async () => {
+			repositoryMock.exist.mockReturnValue(Promise.resolve(false));
+			repositoryMock.update.mockReturnValue(Promise.resolve({ affected: 0 }));
+			expect(await userService.updateUser({ id: user.id }, { isAdmin: true })).toEqual(false);
+			expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
+			expect(repositoryMock.update).not.toHaveBeenCalled();
+		});
 	});
 
-	it("should not update an user and return false because the new user's mail is already used by another user", async () => {
-		repositoryMock.exist.mockReturnValue(Promise.resolve(true));
-		repositoryMock.update.mockReturnValue(
-			Promise.reject(new Error("duplicated mail value violates unique constraint")),
-		);
-		expect(await userService.updateUser({ id: user.id }, { mail: "other.mail@cramptarea.org" })).toEqual(false);
-		expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
-		expect(repositoryMock.update).toHaveBeenCalledWith({ id: user.id }, { mail: "other.mail@cramptarea.org" });
-	});
+	describe("deleteUser", () => {
+		it("should delete an user and return true", async () => {
+			repositoryMock.exist.mockReturnValue(Promise.resolve(true));
+			repositoryMock.delete.mockReturnValue(Promise.resolve({ affected: 1 }));
+			expect(await userService.deleteUser({ id: user.id })).toEqual(true);
+			expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
+			expect(repositoryMock.delete).toHaveBeenCalledWith({ id: user.id });
+		});
 
-	it("should not update an user and return false because the user does not exists", async () => {
-		repositoryMock.exist.mockReturnValue(Promise.resolve(false));
-		repositoryMock.update.mockReturnValue(Promise.resolve({ affected: 0 }));
-		expect(await userService.updateUser({ id: user.id }, { isAdmin: true })).toEqual(false);
-		expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
-		expect(repositoryMock.update).not.toHaveBeenCalled();
-	});
-
-	it("should delete an user and return true", async () => {
-		repositoryMock.exist.mockReturnValue(Promise.resolve(true));
-		repositoryMock.delete.mockReturnValue(Promise.resolve({ affected: 1 }));
-		expect(await userService.deleteUser({ id: user.id })).toEqual(true);
-		expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
-		expect(repositoryMock.delete).toHaveBeenCalledWith({ id: user.id });
-	});
-
-	it("should not delete an user and return false because the user does not exists", async () => {
-		repositoryMock.exist.mockReturnValue(Promise.resolve(false));
-		repositoryMock.delete.mockReturnValue(Promise.resolve({ affected: 0 }));
-		expect(await userService.deleteUser({ id: user.id })).toEqual(false);
-		expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
-		expect(repositoryMock.delete).not.toHaveBeenCalled();
+		it("should not delete an user and return false because the user does not exists", async () => {
+			repositoryMock.exist.mockReturnValue(Promise.resolve(false));
+			repositoryMock.delete.mockReturnValue(Promise.resolve({ affected: 0 }));
+			expect(await userService.deleteUser({ id: user.id })).toEqual(false);
+			expect(repositoryMock.exist).toHaveBeenCalledWith({ where: { id: user.id } });
+			expect(repositoryMock.delete).not.toHaveBeenCalled();
+		});
 	});
 });
