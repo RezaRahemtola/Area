1e96870c44909f4ba97025b43aad884033a63bdd
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-09-25 20:37:21 +0200

    feat(web/basics): add base for each pages of the app (maybe we need a page for the account too)

---
+++ frontend/web/app/(landing)/documentation/page.tsx
@@ -0,0 +1,5 @@
+import DocumentationGeneralHero from "@/components/documentation/GeneralHero";
+
+const LandingPage = () => <DocumentationGeneralHero/>;
+
+export default LandingPage;
--- frontend/web/components/Navbar.tsx
+++ frontend/web/components/Navbar.tsx
@@ -1,32 +1,32 @@
 import ThemeSelector from "@/components/ThemeSelector";
 
 const Navbar = () => (
-	<div className="navbar bg-primary">
-		<div className="navbar-start">
-			<a className="btn btn-ghost normal-case text-xl">Area</a>
-		</div>
-		<div className="navbar-center hidden lg:flex">
-			<ul className="menu menu-horizontal px-1">
-				<li>
-					<a>Features</a>
-				</li>
-				<li>
-					<a>Pricing</a>
-				</li>
-				<li>
-					<a>Documentation</a>
-				</li>
-				<li>
-					<a>Team</a>
-				</li>
-			</ul>
-		</div>
-		<div className="navbar-end">
-			<ThemeSelector />
-			<a className="btn btn-ghost mx-2">Login</a>
-			<a className="btn btn-secondary mr-2">Sign up</a>
-		</div>
-	</div>
+    <div className="navbar bg-primary">
+        <div className="navbar-start">
+            <a className="btn btn-ghost normal-case text-xl" href={"/"}>Area</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+                <li>
+                    <a href={"/features"}>Features</a>
+                </li>
+                <li>
+                    <a href={"/pricing"}>Pricing</a>
+                </li>
+                <li>
+                    <a href={"/documentation"}>Documentation</a>
+                </li>
+                <li>
+                    <a href={"/team"}>Team</a>
+                </li>
+            </ul>
+        </div>
+        <div className="navbar-end">
+            <ThemeSelector/>
+            <a className="btn btn-ghost mx-2">Login</a>
+            <a className="btn btn-secondary mr-2">Sign up</a>
+        </div>
+    </div>
 );
 
 export default Navbar;
