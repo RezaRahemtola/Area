40318f6dfc9e7f898222fe9452b83db460c2d779
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-09-25 20:40:09 +0200

    fix(web/dashboard): fix lint

--- frontend/web/components/dashboard/home/AREAContainer.tsx
+++ frontend/web/components/dashboard/home/AREAContainer.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const AREAContainer = ({selected, setSelected, id}: { selected: number, setSelected: (selected: number) => void, id: number }) => (
-        <div
-            className={`w-1/3 h-1/3 min-w-[188px] ${selected === id ? 'bg-accent' : 'bg-primary'} flex flex-col justify-center items-center`}>
-            {selected === id ?
-                <button className="btn btn-secondary" onClick={() => {
-                    selected === 1 ? setSelected(2) : setSelected(1);
-                }}>
-                    Create new AREA
-                </button> :
-                <div/>}
-        </div>
-    )
+    <div
+        className={`w-1/3 h-1/3 min-w-[188px] ${selected === id ? 'bg-accent' : 'bg-primary'} flex flex-col justify-center items-center`}>
+        {selected === id ?
+            <button className="btn btn-secondary" onClick={() => {
+                setSelected(selected === 1 ? 2 : 1);
+            }}>
+                Create new AREA
+            </button> :
+            <div/>}
+    </div>
+)
 
 
 export default AREAContainer;
