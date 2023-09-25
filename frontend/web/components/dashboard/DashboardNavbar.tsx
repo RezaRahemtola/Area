988a8ae008202e90415ba46285323946935ba7e3
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-09-25 22:30:46 +0200

    fix(web/NavBar): remove NavBar component and replace with div

--- frontend/web/components/dashboard/DashboardNavbar.tsx
+++ frontend/web/components/dashboard/DashboardNavbar.tsx
@@ -1,5 +1,5 @@
 import {Dispatch, SetStateAction} from "react";
-import Navbar from "@/components/Navbar";
+import AuthLayout from "@/layouts/auth/AuthLayout";
 
 type DashboardNavbarProps = {
     isDrawerOpen: boolean;
@@ -7,32 +7,30 @@ type DashboardNavbarProps = {
 };
 
 const DashboardNavbar = ({isDrawerOpen, setIsDrawerOpen}: DashboardNavbarProps) => (
-    <Navbar
-        beforeLogoContent={
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost" onClick={() => setIsDrawerOpen((prev) => !prev)}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="inline-block w-5 h-5 stroke-current"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d={
-                                isDrawerOpen
-                                    ? "M16.29 6.29001L12 10.59L7.71004 6.29001L6.29004 7.71001L10.59 12L6.29004 16.29L7.71004 17.71L12 13.41L16.29 17.71L17.71 16.29L13.41 12L17.71 7.71001L16.29 6.29001Z"
-                                    : "M4 6h16M4 12h16M4 18h16"
-                            }
-                        ></path>
-                    </svg>
-                </button>
-            </div>
-        }
-    />
+    <div className="flex-shrink-0 flex items-center justify-between h-16 px-4 border-b border-gray-200">
+        <button className="btn btn-square btn-ghost" onClick={() => setIsDrawerOpen((prev) => !prev)}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d={
+                        isDrawerOpen
+                            ? "M16.29 6.29001L12 10.59L7.71004 6.29001L6.29004 7.71001L10.59 12L6.29004 16.29L7.71004 17.71L12 13.41L16.29 17.71L17.71 16.29L13.41 12L17.71 7.71001L16.29 6.29001Z"
+                            : "M4 6h16M4 12h16M4 18h16"
+                    }
+                ></path>
+            </svg>
+        </button>
+        <div>
+            <AuthLayout/>
+        </div>
+    </div>
 );
-
 
 export default DashboardNavbar;
