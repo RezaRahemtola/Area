0d2970c97cf9471c74ae4614b1d52db0e5168bbb
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-25 22:42:19 +0200

    fix(web): Navbar switched to main version

--- frontend/web/components/Navbar.tsx
+++ frontend/web/components/Navbar.tsx
@@ -1,35 +1,28 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+
 import ThemeSelector from "@/components/ThemeSelector";
 import AuthLayout from "@/layouts/auth/AuthLayout";
 
-const Navbar = () =>
-    (
-        <div className="navbar bg-primary">
-            <div className="navbar-start">
-                <a className="btn btn-ghost normal-case text-xl" href={"/"}>Area</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li>
-                        <a href={"/features"}>Features</a>
-                    </li>
-                    <li>
-                        <a href={"/pricing"}>Pricing</a>
-                    </li>
-                    <li>
-                        <a href={"/documentation"}>Documentation</a>
-                    </li>
-                    <li>
-                        <a href={"/team"}>Team</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <ThemeSelector/>
-                <AuthLayout/>
-            </div>
-        </div>
+type NavbarProps = {
+	beforeLogoContent?: ReactNode;
+	centerContent?: ReactNode;
+};
 
-    )
-
+const Navbar = ({ beforeLogoContent, centerContent }: NavbarProps) => (
+	<div className="navbar bg-primary">
+		<div className="navbar-start">
+			{beforeLogoContent}
+			<Link className="btn btn-ghost normal-case text-xl" href="/">
+				Area
+			</Link>
+		</div>
+		<div className="navbar-center hidden lg:flex">{centerContent}</div>
+		<div className="navbar-end">
+			<ThemeSelector />
+			<AuthLayout />
+		</div>
+	</div>
+);
 
 export default Navbar;
