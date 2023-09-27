9489119a2386774780d6c6fdb4923b3aebe55e95
Axel Denis <axel.denis@epitech.eu>
2023-09-27 14:43:24 +0200

    refactor(Mobile): separated password re component in it's file

--- frontend/mobile/lib/register.dart
+++ frontend/mobile/lib/register.dart
@@ -1,4 +1,5 @@
 import 'package:area_mobile/Components/email_field.dart';
+import 'package:area_mobile/Components/password_re_field.dart';
 import 'package:area_mobile/login.dart';
 import 'package:area_mobile/main.dart';
 import 'package:area_mobile/Components/password_field.dart';
@@ -41,36 +42,6 @@ class _RegisterState extends State<Register> {
             ],
           ),
         ));
-  }
-}
-
-class PasswordReField extends StatelessWidget {
-  const PasswordReField({
-    super.key,
-    required this.passwordController,
-  });
-
-  final TextEditingController passwordController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
-      child: TextFormField(
-        obscureText: true,
-        controller: passwordController,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: "Re-enter password",
-        ),
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Please re-enter your password.';
-          }
-          return null;
-        },
-      ),
-    );
   }
 }
 
