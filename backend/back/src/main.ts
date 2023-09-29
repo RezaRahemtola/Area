9ae2719915df7bc897dd46f56afd31f14eb05747
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-30 00:28:49 +0200

    fix(back): Remove temporarily proto call

--- backend/back/src/main.ts
+++ backend/back/src/main.ts
@@ -3,18 +3,17 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { version } from "package.json";
-import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.connectMicroservice<MicroserviceOptions>({
-		transport: Transport.GRPC,
-		options: {
-			url: "localhost:50050",
-			package: "area_back",
-			protoPath: "src/grpc/proto/area_back.proto",
-		},
-	});
+	// app.connectMicroservice<MicroserviceOptions>({
+	// 	transport: Transport.GRPC,
+	// 	options: {
+	// 		url: "localhost:50050",
+	// 		package: "area_back",
+	// 		protoPath: "src/grpc/proto/area_back.proto",
+	// 	},
+	// });
 
 	const config = new DocumentBuilder()
 		.setTitle("AREA API")
