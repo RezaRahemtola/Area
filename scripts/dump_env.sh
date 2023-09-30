35d4e0d9b91917f44d1038774c639a1036389097
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-30 21:34:33 +0200

    feat(infisical): Script to dump env in files

---
+++ scripts/dump_env.sh
@@ -0,0 +1,26 @@
+# Creates all the required .env files
+
+# Environment argument parsing
+if [ -z "$1" ]
+  then
+    echo "Please provide an environment (dev or prod)"
+    exit 1
+fi
+INFISICAL_ENVIRONMENT=$1
+
+# Define paths to dump
+paths=( \
+  "frontend/web/" \
+  "backend/back/" \
+)
+file=".env"
+
+# Colors
+green='\033[0;32m'
+clear='\033[0m'
+
+for path in "${paths[@]}"
+  do
+    infisical export --env="$INFISICAL_ENVIRONMENT" --path="$path" > "$path"$file
+    echo -e "${green}Dumped ${path}${file}!${clear}"
+done
