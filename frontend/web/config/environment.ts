ac7e82ddbd5bcdcbf5045423dfd4e7820c658e4e
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-30 18:34:31 +0200

    build(deps): Web dev env injected from infisical

--- frontend/web/config/environment.ts
+++ frontend/web/config/environment.ts
@@ -1,4 +1,2 @@
-import env from "@/utils/environment";
-
 // eslint-disable-next-line import/prefer-default-export
-export const API_URL = env("NEXT_PUBLIC_API_URL", false).default("http://localhost:8080").asString();
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
