99e1cdbcf1728f3244598bceeabd37951066d8f1
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-01 20:31:11 +0200

    feat(web/editor): Basic cards design

---
+++ frontend/web/layouts/editor/ReactionCard.tsx
@@ -0,0 +1,21 @@
+import FontAwesomeIcon from "@/components/FontAwesomeIcon";
+
+const ReactionCard = () => (
+	<div className="card mx-auto w-96 border-primary shadow-2xl">
+		<div className="card-body">
+			<div className="flex">
+				<div className="card shadow-xl">
+					<div className="card-body">
+						<FontAwesomeIcon icon="bolt" />
+					</div>
+				</div>
+				<div className="ml-5">
+					<p className="text-xl">Reaction</p>
+					<p className="">An event a workflow performs after it starts</p>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default ReactionCard;
