25f64e650b6b059fb0fb4bf848f8e13f461ee4f0
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-02 11:16:30 +0200

    feat(web/editor): Action service displayed in summary

--- frontend/web/layouts/editor/action/ActionCard.tsx
+++ frontend/web/layouts/editor/action/ActionCard.tsx
@@ -5,13 +5,14 @@ import { useAtom } from "jotai";
 import { editorWorkflowAtom, selectedEditorArea } from "@/stores/editor";
 import ActionSummaryCard from "@/layouts/editor/action/ActionSummaryCard";
 import ActionSelectServiceCard from "@/layouts/editor/action/ActionSelectServiceCard";
+import { EditorElement } from "@/types/workflows";
 
 enum ActionStep {
 	SUMMARY = 0,
 	SELECT_SERVICE = 1,
 }
 
-const ActionCard = () => {
+const ActionCard = ({ action }: { action: EditorElement }) => {
 	const [workflow] = useAtom(editorWorkflowAtom);
 	const [selectedArea, setSelectedArea] = useAtom(selectedEditorArea);
 	const [step, setStep] = useState<ActionStep>(ActionStep.SUMMARY);
@@ -22,7 +23,7 @@ const ActionCard = () => {
 	};
 
 	if (step === ActionStep.SUMMARY || selectedArea !== workflow.action.id)
-		return <ActionSummaryCard onClick={onSummaryClick} />;
+		return <ActionSummaryCard onClick={onSummaryClick} service={action.service} />;
 	if (step === ActionStep.SELECT_SERVICE) return <ActionSelectServiceCard />;
 
 	return <></>;
--- frontend/web/layouts/editor/action/ActionSelectServiceCard.tsx
+++ frontend/web/layouts/editor/action/ActionSelectServiceCard.tsx
@@ -1,45 +1,77 @@
 import Image from "next/image";
+import { useAtom } from "jotai";
 
 import FontAwesomeIcon from "@/components/FontAwesomeIcon";
+import { editorWorkflowAtom } from "@/stores/editor";
+import { Service } from "@/types/services";
+import { splitArrayInChunks } from "@/utils/arrays";
 
-const ActionServiceElement = () => (
-	<button className="btn btn-ghost normal-case">
+const services: Service[] = [
+	{
+		id: "1",
+		name: "Discord",
+		imageUrl: "https://daisyui.com/tailwind-css-component-profile-2@56w.png",
+		scopes: [],
+	},
+	{
+		id: "2",
+		name: "GitHub",
+		imageUrl: "https://daisyui.com/tailwind-css-component-profile-3@56w.png",
+		scopes: [],
+	},
+	{
+		id: "3",
+		name: "Google",
+		imageUrl: "https://daisyui.com/tailwind-css-component-profile-4@56w.png",
+		scopes: [],
+	},
+	{
+		id: "4",
+		name: "Twitter",
+		imageUrl: "https://daisyui.com/tailwind-css-component-profile-5@56w.png",
+		scopes: [],
+	},
+];
+
+const ActionServiceElement = ({ service, onClick }: { service: Service; onClick: (service: Service) => void }) => (
+	<button className="btn btn-ghost normal-case" onClick={() => onClick(service)}>
 		<div className="flex">
 			<div className="avatar">
 				<div className="mask mask-squircle w-8 h-8">
-					<Image
-						src="https://daisyui.com/tailwind-css-component-profile-2@56w.png"
-						alt="Service logo"
-						width={500}
-						height={500}
-					/>
+					<Image src={service.imageUrl} alt="Service logo" width={500} height={500} />
 				</div>
 			</div>
-			<p className="text-xl font-semibold ml-2">GitHub</p>
+			<p className="text-xl font-semibold ml-2">{service.name}</p>
 		</div>
 	</button>
 );
-const ActionSelectServiceCard = () => (
-	<div className="card mx-auto w-2/3 shadow-2xl">
-		<div className="card-body items-center pt-3">
-			<div className="flex mb-5">
-				<FontAwesomeIcon icon="bolt" svgProps={{ className: "h-7 w-7" }} />
-				<p className="card-title text-2xl ml-2">Action</p>
-			</div>
+const ActionSelectServiceCard = () => {
+	const [, setWorkflow] = useAtom(editorWorkflowAtom);
 
-			<div className="flex w-full justify-around my-2">
-				<ActionServiceElement />
-				<ActionServiceElement />
-				<ActionServiceElement />
-			</div>
+	const servicesChunks = splitArrayInChunks(services, 3);
 
-			<div className="flex w-full justify-around my-2">
-				<ActionServiceElement />
-				<ActionServiceElement />
-				<ActionServiceElement />
+	const onServiceClick = (service: Service) => {
+		setWorkflow((prev) => ({ ...prev, action: { ...prev.action, service } }));
+	};
+
+	return (
+		<div className="card mx-auto w-2/3 shadow-2xl">
+			<div className="card-body items-center pt-3">
+				<div className="flex mb-5">
+					<FontAwesomeIcon icon="bolt" svgProps={{ className: "h-7 w-7" }} />
+					<p className="card-title text-2xl ml-2">Action</p>
+				</div>
+
+				{servicesChunks.map((chunk, index) => (
+					<div className="flex w-full justify-around my-2" key={index}>
+						{chunk.map((service) => (
+							<ActionServiceElement service={service} onClick={onServiceClick} key={service.id} />
+						))}
+					</div>
+				))}
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default ActionSelectServiceCard;
