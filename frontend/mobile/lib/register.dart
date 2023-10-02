55a2134713044537def3d5dc789c77bf376149e6
Axel DENIS <axel.denis@epitech.eu>
2023-10-02 18:10:03 +0200

    feat(Mobile) added register function to call the back

--- frontend/mobile/lib/register.dart
+++ frontend/mobile/lib/register.dart
@@ -1,8 +1,10 @@
 import 'package:area_mobile/Components/email_field.dart';
 import 'package:area_mobile/Components/password_re_field.dart';
 import 'package:area_mobile/main.dart';
+import 'package:dio/dio.dart';
 import 'package:area_mobile/Components/password_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key, required this.title});
@@ -73,13 +75,28 @@ class RegisterButton extends StatelessWidget {
                 const SnackBar(content: Text("Invalid email.")),
               );
             } else {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => HomePage(
-                          email: emailController.text,
-                        )),
-              );
+              registerAccount(
+                  emailController.text, passwordController.text, context,
+                  (value) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      content:
+                          Text("Registered")),
+                );
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => HomePage(
+                            email: emailController.text,
+                          )),
+                );
+              }, () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      content:
+                          Text("Invalid/Existing Email / Password too simple")),
+                );
+              });
             }
           } else {
             ScaffoldMessenger.of(context).showSnackBar(
@@ -89,4 +106,28 @@ class RegisterButton extends StatelessWidget {
         },
         child: const Text("Submit"));
   }
+}
+
+Future<void> registerAccount(email, password, BuildContext context,
+    ValueChanged<Response<dynamic>> onSuccess, VoidCallback onFail) async {
+  var dio = Dio();
+  try {
+    var response = await dio
+        .post('http://${dotenv.env['BACKIP']}/auth/register/',
+            data: {"email": email, "password": password},
+            options: Options(
+              followRedirects: true,
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+              },
+            ))
+        .timeout(const Duration(seconds: 3));
+    if (response.statusCode == 200) {
+      onSuccess.call(response);
+    }
+  } catch (e) {
+    print(e.toString());
+  }
+  onFail.call();
 }
