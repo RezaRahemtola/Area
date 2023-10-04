cb4b846e6cde329c72cb971e5d3e78098b40dcf5
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-04 13:48:03 +0200

    feat(mobile/services): Services API calls

--- frontend/mobile/lib/types/services.dart
+++ frontend/mobile/lib/types/services.dart
@@ -7,3 +7,28 @@ class ServiceReturn<T> {
     this.error,
   });
 }
+
+class Service {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final List<String> scopes;
+
+  const Service({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.scopes,
+  });
+}
+
+class Area {
+  final String id;
+  final String name;
+  final List<String> serviceScopesNeeded;
+
+  const Area(
+      {required this.id,
+      required this.name,
+      required this.serviceScopesNeeded});
+}
