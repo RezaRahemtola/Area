a9cdd35b07eb6386a0ed143ddc6eea99dfd027ca
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-04 13:50:41 +0200

    fix(mobile/auth): Response typing

--- frontend/mobile/lib/services/auth/register.dart
+++ frontend/mobile/lib/services/auth/register.dart
@@ -1,13 +1,14 @@
 import 'package:area_mobile/services/dio.dart';
 import 'package:area_mobile/types/services.dart';
+import 'package:area_mobile/types/services/auth.dart';
 
 Future<ServiceReturn<String>> register(String email, String password) async {
   try {
-    final response = await dio.post(
+    final response = await dio.post<AuthResponse>(
       '/auth/register',
       data: {"email": email, "password": password},
     );
-    return ServiceReturn(data: response.data["accessToken"]);
+    return ServiceReturn(data: response.data!.accessToken);
   } catch (e) {
     return ServiceReturn(error: e.toString());
   }
