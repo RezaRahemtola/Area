6594c613eb655508d08a96b19c3aecc514163d3e
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-05 23:13:18 +0200

    refactor(web/editor): Action and reaction cards

--- frontend/web/layouts/editor/EditorCards.tsx
+++ frontend/web/layouts/editor/EditorCards.tsx
@@ -4,18 +4,19 @@ import { useAtom } from "jotai";
 
 import { editorWorkflowAtom } from "@/stores/editor";
 import EditorSeparator from "@/components/editor/EditorSeparator";
-import EditorCard from "@/layouts/editor/EditorCard";
+import ActionCard from "@/components/editor/action/ActionCard";
+import ReactionCard from "@/components/editor/reaction/ReactionCard";
 
 const EditorCards = () => {
 	const [workflow] = useAtom(editorWorkflowAtom);
 
 	return (
 		<div className="bg-neutral">
-			<EditorCard area={workflow.action} isAction />
+			<ActionCard />
 			<EditorSeparator index={0} />
 			{workflow.reactions.map((reaction, index) => (
 				<div key={reaction.id}>
-					<EditorCard area={reaction} isAction={false} />
+					<ReactionCard reaction={reaction} />
 					<EditorSeparator index={index + 1} />
 				</div>
 			))}
