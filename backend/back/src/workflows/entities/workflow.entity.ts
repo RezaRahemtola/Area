f9ac853e40abab6df6587017dd43b2aa74232457
Croos3r <dorian.moy@epitech.eu>
2023-10-07 01:03:46 +0200

    docs(workflow-service): made workflows service documentation
    Took 48 minutes

--- backend/back/src/workflows/entities/workflow.entity.ts
+++ backend/back/src/workflows/entities/workflow.entity.ts
@@ -11,28 +11,47 @@ import {
 } from "typeorm";
 import { User } from "../../users/entities/user.entity";
 import WorkflowArea from "./workflow-area.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 @Unique(["name", "ownerId"])
 export default class Workflow {
+	@ApiProperty()
 	@PrimaryGeneratedColumn("uuid")
 	id!: string;
 
+	@ApiProperty()
 	@Column()
 	name!: string;
 
 	@Column()
 	ownerId!: string;
+	@ApiProperty({
+		type: () => User,
+	})
 	@ManyToOne(() => User, { nullable: false })
 	@JoinColumn({ name: "owner_id" })
 	owner!: User;
 
+	@ApiProperty()
 	@Column()
 	active!: boolean;
 
+	@ApiProperty({
+		example: {
+			id: "string",
+			parameters: {},
+		},
+	})
 	@OneToMany(() => WorkflowArea, (reaction) => reaction.workflow, { cascade: true, nullable: true })
 	reactions!: WorkflowArea[];
 
+	@ApiProperty({
+		example: {
+			id: "string",
+			parameters: {},
+		},
+	})
 	@OneToOne(() => WorkflowArea, (action) => action.actionOfWorkflow, {
 		cascade: true,
 		nullable: true,
@@ -41,6 +60,7 @@ export default class Workflow {
 	@JoinColumn()
 	action!: WorkflowArea;
 
+	@ApiProperty()
 	@CreateDateColumn()
 	createdAt!: Date;
 }
