14111f757f580f9d095736b863993f09d17919f6
Croos3r <dorian.moy@epitech.eu>
2023-10-06 23:41:35 +0200

    feat(workflow-service): made update workflow endpoint and service function
    Took 38 minutes

--- backend/back/src/workflows/workflows.service.ts
+++ backend/back/src/workflows/workflows.service.ts
@@ -6,6 +6,7 @@ import WorkflowStep from "./entities/workflow-step.entity";
 import WorkflowStepDto, { WorkflowEntryDto } from "./dto/workflow-step.dto";
 import Area from "../services/entities/area.entity";
 import { User } from "../users/entities/user.entity";
+import UpdateWorkflowDto from "./dto/update-workflow.dto";
 
 @Injectable()
 export class WorkflowsService {
@@ -72,6 +73,51 @@ export class WorkflowsService {
 		return {
 			id,
 		};
+	}
+
+	async updateWorkflow(workflowId: string, { name, reactions, action }: UpdateWorkflowDto, ownerId: string) {
+		const workflow = await this.workflowRepository.findOne({
+			where: {
+				id: workflowId,
+			},
+			relations: {
+				action: true,
+				reactions: true,
+			},
+		});
+		if (!workflow) throw new NotFoundException(`Workflow ${workflowId} not found.`);
+		if (!name && !action && !reactions) return false;
+		let result = false;
+
+		if (name) {
+			if (await this.workflowRepository.exist({ where: { name, ownerId: ownerId } }))
+				throw new ConflictException(`Workflow ${name} already exists.`);
+			result ||= (await this.workflowRepository.update(workflowId, { name })).affected > 0;
+		}
+
+		if (action) {
+			const { id, areaId, areaServiceId, parameters } = action;
+			if (id !== workflow.action.id) {
+				throw new ConflictException(
+					`You can only change the action ${workflow.action.id} for the workflow ${workflowId}.`,
+				);
+			}
+			result ||=
+				(
+					await this.workflowStepRepository.update(id, {
+						area: { id: areaId, serviceId: areaServiceId },
+						parameters,
+					})
+				).affected > 0;
+		}
+
+		if (reactions) {
+			await this.workflowStepRepository.delete({ id: In(workflow.reactions.map((reaction) => reaction.id)) });
+			const reactionsToSave = await this.createWorkflowSteps(workflow.action, reactions, workflow);
+			result ||= (await this.workflowRepository.update(workflowId, { reactions: reactionsToSave })).affected > 0;
+		}
+
+		return result;
 	}
 
 	async toggleWorkflows(workflows: string[], newState: boolean, ownerId: string) {
