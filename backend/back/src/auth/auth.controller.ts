109ede6bfca0f199321eadfb358a962ffcc4a213
Croos3r <dorian.moy@epitech.eu>
2023-10-07 22:26:27 +0200

    fix(auth-service): removed strong password check in login dto
    Took 4 minutes

--- backend/back/src/auth/auth.controller.ts
+++ backend/back/src/auth/auth.controller.ts
@@ -1,18 +1,18 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import CredentialsDto from "./dto/credentials.dto";
+import { LoginCredentialsDto, RegisterCredentialsDto } from "./dto/credentials.dto";
 
 @Controller("auth")
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post("login")
-	async logIn(@Body() { email, password }: CredentialsDto) {
+	async logIn(@Body() { email, password }: LoginCredentialsDto) {
 		return this.authService.logIn(email, password);
 	}
 
 	@Post("register")
-	async register(@Body() { email, password }: CredentialsDto) {
+	async register(@Body() { email, password }: RegisterCredentialsDto) {
 		return this.authService.register(email, password);
 	}
 }
