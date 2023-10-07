e02bf1b99ac12605fe75eb44a68f44ec2bd87ae5
Croos3r <dorian.moy@epitech.eu>
2023-10-07 19:19:46 +0200

    feat(migrations-and-seeding): made base tables creation and seeding of services and service scopes for the only current service github
    Took 1 hour 47 minutes

--- backend/back/src/app.module.ts
+++ backend/back/src/app.module.ts
@@ -1,21 +1,14 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 import { GrpcModule } from "./grpc/grpc.module";
-import { User } from "./users/entities/user.entity";
 import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
 import { JobsModule } from "./jobs/jobs.module";
 import { ConnectionsModule } from "./connections/connections.module";
 import { ServicesModule } from "./services/services.module";
 import { WorkflowsModule } from "./workflows/workflows.module";
-import Service from "./services/entities/service.entity";
-import ServiceScope from "./services/entities/service-scope.entity";
-import UserConnection from "./connections/entities/user-connection.entity";
-import Area from "./services/entities/area.entity";
-import Workflow from "./workflows/entities/workflow.entity";
-import WorkflowArea from "./workflows/entities/workflow-area.entity";
+import { DATA_SOURCE_OPTIONS } from "./database";
 
 @Module({
 	imports: [
@@ -23,22 +16,7 @@ import WorkflowArea from "./workflows/entities/workflow-area.entity";
 			envFilePath: [".env"],
 			isGlobal: true,
 		}),
-		TypeOrmModule.forRootAsync({
-			imports: [ConfigModule],
-			useFactory: (config: ConfigService) => ({
-				type: "postgres",
-				namingStrategy: new SnakeNamingStrategy(),
-				host: config.getOrThrow<string>("DB_HOST"),
-				port: config.getOrThrow<number>("DB_PORT"),
-				username: config.getOrThrow<string>("POSTGRES_USER"),
-				password: config.getOrThrow<string>("POSTGRES_PASSWORD"),
-				database: config.getOrThrow<string>("POSTGRES_DB"),
-				entities: [User, Service, ServiceScope, UserConnection, Area, Workflow, WorkflowArea],
-				migrations: [],
-				synchronize: config.getOrThrow<string>("NODE_ENV") === "development",
-			}),
-			inject: [ConfigService],
-		}),
+		TypeOrmModule.forRoot(DATA_SOURCE_OPTIONS),
 		AuthModule,
 		ConnectionsModule,
 		GrpcModule,
