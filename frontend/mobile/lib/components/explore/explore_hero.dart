b5042840fbb153f080a7c4ef0b6f294a744ad8cf
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-10-08 00:10:10 +0200

    feat(front/mobile): fetch API data for services and display it on Explore section

--- frontend/mobile/lib/components/explore/explore_hero.dart
+++ frontend/mobile/lib/components/explore/explore_hero.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ExploreHero extends StatelessWidget {
   const ExploreHero({Key? key}) : super(key: key);
+
+  Future<List<Map<String, dynamic>>> fetchServices() async {
+    final apiUrl = Uri.parse("${dotenv.env['API_URL']}/services");
+
+    final response = await http.get(apiUrl);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return List<Map<String, dynamic>>.from(data);
+    } else {
+      throw Exception('Failed to load services');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,56 +27,43 @@ class ExploreHero extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: const <Widget>[
-            ServiceTile(
-              serviceName: 'Spotify',
-              serviceIcon: Icons.music_note,
-            ),
-            Divider(),
-            ServiceTile(
-              serviceName: 'Discord',
-              serviceIcon: Icons.message,
-            ),
-            Divider(),
-            ServiceTile(
-              serviceName: 'YouTube',
-              serviceIcon: Icons.video_library,
-            ),
-            Divider(),
-            ServiceTile(
-              serviceName: 'Gmail',
-              serviceIcon: Icons.email,
-            ),
-            Divider(),
-          ],
+        child: FutureBuilder<List<Map<String, dynamic>>>(
+          future: fetchServices(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              final List<Map<String, dynamic>> services = snapshot.data ?? [];
+              return ListView.builder(
+                itemCount: services.length,
+                itemBuilder: (context, index) {
+                  final service = services[index];
+                  final serviceName = service['id'];
+                  final serviceImage = service['imageUrl'];
+
+                  return Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: Image.network(
+                          serviceImage,
+                          width: 40,
+                          height: 40,
+                          fit: BoxFit.cover,
+                        ),
+                        title: Text(serviceName),
+                        onTap: () {},
+                      ),
+                      const Divider(),
+                    ],
+                  );
+                },
+              );
+            }
+          },
         ),
       ),
-    );
-  }
-}
-
-class ServiceTile extends StatelessWidget {
-  final String serviceName;
-  final IconData serviceIcon;
-
-  const ServiceTile({
-    required this.serviceName,
-    required this.serviceIcon,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(
-        serviceIcon,
-        color: Colors.blue,
-      ),
-      title: Text(serviceName),
-      onTap: () {
-        // Open modal with service Properties
-      },
     );
   }
 }
