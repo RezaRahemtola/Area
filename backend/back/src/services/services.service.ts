efc559ee930c99abcb7cda7ccad9646268934f35
Croos3r <dorian.moy@epitech.eu>
2023-10-09 01:21:38 +0200

    fix(services-service): weird typing now refactored
    Took 2 hours 34 minutes

--- backend/back/src/services/services.service.ts
+++ backend/back/src/services/services.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import Service from "./entities/service.entity";
 import { Repository } from "typeorm";
-import ServiceDto from "./dto/service.dto";
 import Area from "./entities/area.entity";
 import { AreaDto } from "./dto/area.dto";
 import { ServiceHasFilter } from "./dto/service-query-filter.dto";
@@ -33,14 +32,22 @@ export class ServicesService {
 		);
 	}
 
-	async getService(id: string, withScopes: boolean = false): Promise<ServiceDto | Service | null> {
+	async getService(
+		id: string,
+		withScopes: boolean = false,
+	): Promise<
+		| (Omit<Service, "scopes"> & {
+				scopes?: string[];
+		  })
+		| null
+	> {
 		const service = await this.serviceRepository.findOne({ where: { id }, relations: { scopes: withScopes } });
 		if (!service) return null;
 		if (withScopes) {
 			const { scopes, ...serviceDto } = service;
 			return { ...serviceDto, scopes: scopes.map(({ id }) => id) };
 		}
-		return service;
+		return service as Omit<Service, "scopes">;
 	}
 
 	async getAREAsForService(id: string, action: boolean): Promise<AreaDto[] | null> {
