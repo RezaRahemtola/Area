cec766cbc13745bd8247bc3a5921e344d837401c
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-08 19:47:01 +0200

    feat(web/editor): Action and reaction services fetching

--- frontend/web/layouts/editor/EditorSelectServiceCard.tsx
+++ frontend/web/layouts/editor/EditorSelectServiceCard.tsx
@@ -5,46 +5,26 @@ import EditorStepCardWrapper from "@/components/editor/EditorStepCardWrapper";
 import { EditorCardActions } from "@/types/editor";
 import { EditorWorkflowElementService } from "@/types/workflows";
 
-const services: Service[] = [
-	{
-		id: "Discord",
-		imageUrl: "https://daisyui.com/tailwind-css-component-profile-2@56w.png",
-		scopes: [],
-		oauthUrl: "",
-	},
-	{
-		id: "GitHub",
-		imageUrl: "https://daisyui.com/tailwind-css-component-profile-3@56w.png",
-		scopes: [],
-		oauthUrl: "",
-	},
-	{
-		id: "Google",
-		imageUrl: "https://daisyui.com/tailwind-css-component-profile-4@56w.png",
-		scopes: [],
-		oauthUrl: "",
-	},
-	{
-		id: "Twitter",
-		imageUrl: "https://daisyui.com/tailwind-css-component-profile-5@56w.png",
-		scopes: [],
-		oauthUrl: "",
-	},
-];
-
 type EditorSelectServiceCardProps = {
 	title: string;
 	actions: EditorCardActions;
 	currentService?: EditorWorkflowElementService;
+	serviceChoices: Service[];
 	onNextStep: (service: Service) => void;
 };
-const EditorSelectServiceCard = ({ title, actions, currentService, onNextStep }: EditorSelectServiceCardProps) => {
+const EditorSelectServiceCard = ({
+	title,
+	actions,
+	currentService,
+	serviceChoices,
+	onNextStep,
+}: EditorSelectServiceCardProps) => {
 	const [selectedServiceId, setSelectedServiceId] = useState<string | undefined>(currentService?.id);
 
 	return (
 		<EditorStepCardWrapper title={title} actions={actions}>
 			<ServicesList
-				services={services}
+				services={serviceChoices}
 				nbPerLine={3}
 				selectedServiceId={selectedServiceId}
 				setSelectedService={setSelectedServiceId}
@@ -54,7 +34,7 @@ const EditorSelectServiceCard = ({ title, actions, currentService, onNextStep }:
 				<button
 					className="btn btn-primary btn-wide disabled:bg-accent"
 					disabled={!selectedServiceId}
-					onClick={() => onNextStep(services.find((s) => s.id === selectedServiceId)!)}
+					onClick={() => onNextStep(serviceChoices.find((s) => s.id === selectedServiceId)!)}
 				>
 					Next
 				</button>
