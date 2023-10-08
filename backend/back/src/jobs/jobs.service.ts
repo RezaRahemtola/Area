8a39b7c4d62eaa89e63a5c3241ee0a81a850a54e
Croos3r <dorian.moy@epitech.eu>
2023-10-08 21:55:31 +0200

    feat(jobs): made function to retrieve the jobs to be executed (start and next), basically the job of @EdenComp
    Took 1 minute

--- backend/back/src/jobs/jobs.service.ts
+++ backend/back/src/jobs/jobs.service.ts
@@ -4,15 +4,36 @@ import { LaunchJobDto } from "./jobs.dto";
 import { JobParamsClasses, JobsParams, JobsType } from "../types/jobs";
 import { validate } from "class-validator";
 import { plainToInstance } from "class-transformer";
+import { WorkflowsService } from "../workflows/workflows.service";
+import { InjectRepository } from "@nestjs/typeorm";
+import WorkflowArea from "../workflows/entities/workflow-area.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class JobsService {
-	constructor(private readonly grpcService: GrpcService) {}
+	constructor(
+		private readonly grpcService: GrpcService,
+		private readonly workflowsService: WorkflowsService,
+		@InjectRepository(WorkflowArea)
+		private readonly workflowAreaRepository: Repository<WorkflowArea>,
+	) {}
 
-	convertParams = async <TJobs extends JobsType>(
-		job: JobsType,
-		params: unknown,
-	): Promise<JobsParams["mappings"][TJobs]> => {
+	async getActionJobsToStart() {
+		return (await this.workflowsService.getWorkflowsWithAreas(undefined, true)).map(
+			({ action: { jobId, parameters } }) => ({ jobId, params: parameters }),
+		);
+	}
+
+	async getReactionsForJob(jobId: string) {
+		return (
+			await this.workflowAreaRepository.find({
+				where: { jobId },
+				relations: { nextWorkflowReactions: true },
+			})
+		).map(({ nextWorkflowReactions }) => nextWorkflowReactions.map(({ jobId, parameters }) => ({ jobId, parameters })));
+	}
+
+	async convertParams<TJobs extends JobsType>(job: JobsType, params: unknown): Promise<JobsParams["mappings"][TJobs]> {
 		const data = plainToInstance<object, unknown>(JobParamsClasses[job], params);
 		let errors = [];
 
@@ -32,7 +53,7 @@ export class JobsService {
 			throw new BadRequestException(`Invalid job parameters: ${message}`);
 		}
 		return data as JobsParams["mappings"][TJobs];
-	};
+	}
 
 	async launchJob(job: LaunchJobDto): Promise<void> {
 		const params = await this.convertParams(job.job, job.params);
