77ae0630558b9eb64cfa623a169c58694a3bd0e3
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-08 15:33:58 +0200

    feat(web/workflows): Saving

--- frontend/web/services/workflows/editor.ts
+++ frontend/web/services/workflows/editor.ts
@@ -8,14 +8,34 @@ type CreateWorkflowReturn = {
 };
 export const create = async (workflow: EditorWorkflow): Promise<ServiceReturn<CreateWorkflowReturn>> => {
 	try {
-		const response = await axiosInstance.post<CreateWorkflowReturn>(`/workflows`, { data: workflow });
+		const response = await axiosInstance.post<CreateWorkflowReturn>(`/workflows`, {
+			data: {
+				name: workflow.name,
+				active: workflow.active,
+				action: {
+					id: workflow.action.id,
+					parameters: workflow.action.parameters,
+					areaId: workflow.action.area?.id,
+					areaServiceId: workflow.action.areaService?.id,
+				},
+				reactions: workflow.reactions.map((reaction) => ({
+					id: reaction.id,
+					parameters: reaction.parameters,
+					areaId: reaction.area?.id,
+					areaServiceId: reaction.areaService?.id,
+					previousAreaId: reaction.previousAreaId,
+				})),
+			},
+		});
 		return { data: response.data, error: undefined };
 	} catch (error) {
 		return { data: null, error: SERVICE_ERROR_UNKNOWN };
 	}
 };
 
-export const update = async (workflow: Partial<EditorWorkflow>): Promise<ServiceReturn<CreateWorkflowReturn>> => {
+export const update = async (
+	workflow: Partial<EditorWorkflow> & { id: string },
+): Promise<ServiceReturn<CreateWorkflowReturn>> => {
 	try {
 		const response = await axiosInstance.patch<CreateWorkflowReturn>(`/workflows/${workflow.id}`, { data: workflow });
 		return { data: response.data, error: undefined };
