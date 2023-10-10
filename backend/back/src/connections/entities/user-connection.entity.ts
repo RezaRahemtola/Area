25a62b6f4fdc30532aa3fa3b0733a391888f8682
Croos3r <dorian.moy@epitech.eu>
2023-10-10 14:12:23 +0200

    feat(user-connection): made user connection data column ts type any instead of unknown to be able to use it without validation
    Took 1 hour 20 minutes

--- backend/back/src/connections/entities/user-connection.entity.ts
+++ backend/back/src/connections/entities/user-connection.entity.ts
@@ -20,7 +20,8 @@ export default class UserConnection {
 	scopes!: ServiceScope[];
 
 	@Column({ type: "jsonb" })
-	data!: unknown;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	data!: any;
 
 	@CreateDateColumn()
 	createdAt!: Date;
