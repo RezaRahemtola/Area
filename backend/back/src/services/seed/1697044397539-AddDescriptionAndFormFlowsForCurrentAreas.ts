e829f2eafa366d67de38b940bab951e20d6c0216
Croos3r <dorian.moy@epitech.eu>
2023-10-11 19:30:05 +0200

    chore(areas-parameters-form-flow): updated existing areas with adequate parameters form flows and description
    Took 43 seconds

---
+++ backend/back/src/services/seed/1697044397539-AddDescriptionAndFormFlowsForCurrentAreas.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import Area from "../entities/area.entity";
+
+export class AddDescriptionAndFormFlowsForCurrentAreas1697044397539 implements MigrationInterface {
+	private readonly descriptionsAndFormFlows: Array<Partial<Area>> = [
+		{
+			id: "send-email",
+			serviceId: "google",
+			description: "Send an email with Google GMAIL",
+			parametersFormFlow: [
+				{
+					for: "to",
+					type: "email",
+					required: true,
+				},
+				{
+					for: "subject",
+					type: "short-text",
+					required: true,
+				},
+				{
+					for: "body",
+					type: "long-text",
+					required: true,
+				},
+			],
+		},
+		{
+			id: "seconds-interval",
+			serviceId: "timer",
+			description: "Trigger every X seconds",
+			parametersFormFlow: [
+				{
+					for: "seconds",
+					type: "integer",
+					required: true,
+				},
+			],
+		},
+	];
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.manager.getRepository(Area).save(this.descriptionsAndFormFlows);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.manager.getRepository(Area).update(
+			this.descriptionsAndFormFlows.map(({ id }) => id),
+			{
+				description: "An area description",
+				parametersFormFlow: [],
+			},
+		);
+	}
+}
