4a77ff558d4aac2c1622eca41c939a1e23eadc6a
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-13 13:32:06 +0200

    feat(web/translations): Replacements started

--- frontend/web/locales/en-US.ts
+++ frontend/web/locales/en-US.ts
@@ -2,6 +2,18 @@ const translations = {
 	dashboard: {
 		title: "Dashboard",
 	},
+	library: {
+		emptyState: {
+			title: "You don't have any workflow",
+			action: "Create one",
+		},
+	},
+	actions: {
+		rename: "Rename",
+		delete: "Delete",
+		save: "Save",
+		close: "Close",
+	},
 };
 
 export default translations;
