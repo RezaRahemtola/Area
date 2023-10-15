1c62546be240ed40faefb04bbb3e0e267656ed33
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-15 20:50:40 +0200

    feat(workers): Create draft email reaction (#101)

--- backend/supervisor/jobs/images.go
+++ backend/supervisor/jobs/images.go
@@ -3,11 +3,13 @@ package jobs
 var JobToImage = map[string]string{
 	"timer-seconds-interval": "supervisor-seconds-interval",
 	"google-send-email":      "supervisor-gmail",
+	"google-create-draft-email":      "supervisor-gmail",
 }
 
 var OptArgument = map[string]string{
 	"timer-seconds-interval": "",
 	"google-send-email":      "send-email",
+	"google-create-draft-email":      "create-draft-email",
 }
 
 func GetImages() []string {
