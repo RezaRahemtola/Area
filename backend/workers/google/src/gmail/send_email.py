782b255baaba880c3b4fa292d9032b4448cbfc39
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-16 00:58:18 +0200

    feat(worker/google): Duplicated code reduced

--- backend/workers/google/src/gmail/send_email.py
+++ backend/workers/google/src/gmail/send_email.py
@@ -1,12 +1,9 @@
 import base64
 import json
-import os
-import sys
 
 from email.message import EmailMessage
 
 import grpc
-from google.oauth2.credentials import Credentials
 from google.protobuf.struct_pb2 import Struct
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -14,33 +11,17 @@ from googleapiclient.errors import HttpError
 from area_back_pb2_grpc import AreaBackServiceStub
 from area_types_pb2 import JobData
 
+from src.utils.auth import forge_credentials
+from src.utils.parsing import get_arguments
+
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 TARGET = "localhost:50050"
 
 def send_email():
-    args = {}
-    for i in range(2, len(sys.argv)):
-        if sys.argv[i].startswith("--"):
-            if not sys.argv[i + 1]:
-                print(F"Error: No value specified for {sys.argv[i]}")
-                exit(1)
-            args[sys.argv[i][2:]] = sys.argv[i + 1]
-            i += 1
-
-    keys_needed = {"auth", "to", "subject", "body", "workflowStepId"}
-    missing = keys_needed.difference(args.keys())
-    if missing:
-        print(F"Error: Missing required keys: {missing}")
-        exit(1)
+    args = get_arguments({"auth", "to", "subject", "body", "workflowStepId"})
 
     credentials = json.loads(args["auth"])
-
-    creds = Credentials.from_authorized_user_info(info={
-        "refresh_token": credentials["refresh_token"],
-        "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
-        "client_id": os.getenv("GOOGLE_CLIENT_ID"),
-        "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
-    }, scopes=SCOPES)
+    creds = forge_credentials(credentials["refresh_token"], SCOPES)
 
     try:
         service = build('gmail', 'v1', credentials=creds)
--- backend/workers/google/src/youtube/create_comment.py
+++ backend/workers/google/src/youtube/create_comment.py
@@ -1,14 +1,12 @@
 import json
-import os
-import sys
-
-from email.message import EmailMessage
 
 import grpc
-from google.oauth2.credentials import Credentials
 from google.protobuf.struct_pb2 import Struct
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+from src.utils.auth import forge_credentials
+from src.utils.parsing import get_arguments
 
 from area_back_pb2_grpc import AreaBackServiceStub
 from area_types_pb2 import JobData
@@ -18,29 +16,10 @@ SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 TARGET = "localhost:50050"
 
 def create_comment():
-    args = {}
-    for i in range(2, len(sys.argv)):
-        if sys.argv[i].startswith("--"):
-            if not sys.argv[i + 1]:
-                print(F"Error: No value specified for {sys.argv[i]}")
-                exit(1)
-            args[sys.argv[i][2:]] = sys.argv[i + 1]
-            i += 1
-
-    keys_needed = {"auth", "videoId", "content", "workflowStepId"}
-    missing = keys_needed.difference(args.keys())
-    if missing:
-        print(F"Error: Missing required keys: {missing}")
-        exit(1)
+    args = get_arguments({"auth", "videoId", "content", "workflowStepId"})
 
     credentials = json.loads(args["auth"])
-
-    creds = Credentials.from_authorized_user_info(info={
-        "refresh_token": credentials["refresh_token"],
-        "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
-        "client_id": os.getenv("GOOGLE_CLIENT_ID"),
-        "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
-    }, scopes=SCOPES)
+    creds = forge_credentials(credentials["refresh_token"], SCOPES)
 
     try:
         service = build('youtube', 'v3', credentials=creds)
