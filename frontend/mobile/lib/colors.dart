2973dc9ae0d83c61fd0da0509059649b2b5426aa
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-10-17 17:41:05 +0200

    feat(front/mobile): add theme + switch bottom bar color

---
+++ frontend/mobile/lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFC5C6C6);
+const secondaryColor = Color(0xFFF1895C);
+const accentColor = Color(0xFFFFFFFF);
+const neutralColor = Color(0xFF516079);
