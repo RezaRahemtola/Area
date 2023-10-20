a7f5ec4468c1f9d3783a905b0bf1180c3366be47
Croos3r <dorian.moy@epitech.eu>
2023-10-20 09:00:47 +0200

    chore(logging): added logs in auth service
    Took 21 minutes

--- backend/back/src/auth/auth.service.ts
+++ backend/back/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConflictException, Injectable, Logger, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from "../users/users.service";
 import { hash, verify as verifyArgonHash } from "argon2";
 import { JwtService } from "@nestjs/jwt";
@@ -7,6 +7,8 @@ import { JwtPayload } from "../types/jwt";
 
 @Injectable()
 export class AuthService {
+	private readonly logger = new Logger(AuthService.name);
+
 	constructor(
 		private readonly jwtService: JwtService,
 		private readonly usersService: UsersService,
@@ -15,14 +17,14 @@ export class AuthService {
 	async logIn(email: string, password: string): Promise<LoginResultDto> {
 		try {
 			const user = await this.usersService.getUser({ email });
-			if (!(await verifyArgonHash(user.passwordHash, password))) {
-				// noinspection ExceptionCaughtLocallyJS
-				throw new UnauthorizedException("Invalid password.");
-			}
+			// noinspection ExceptionCaughtLocallyJS
+			if (!(await verifyArgonHash(user.passwordHash, password))) throw new UnauthorizedException("Invalid password.");
+			this.logger.log(`Logging in user ${email}`);
 			return {
 				accessToken: this.jwtService.sign({ id: user.id }),
 			};
 		} catch (error) {
+			this.logger.warn(`Invalid credentials for user ${email}`);
 			throw new UnauthorizedException("Invalid email/password");
 		}
 	}
@@ -31,6 +33,7 @@ export class AuthService {
 		const isUserCreated = await this.usersService.createUser(email, await hash(password));
 		if (!isUserCreated) throw new ConflictException("An user already exists with this email.");
 		const { id } = await this.usersService.getUser({ email });
+		this.logger.log(`Registered user ${email}, logging them in automatically`);
 		return {
 			accessToken: this.jwtService.sign({ id }),
 		};
