02bfbe10be9687fc13ea4d966b009e01b38979b4
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-10-21 21:47:33 +0200

    fix(front/mobile): add token to request + remove easy_search_bar

--- frontend/mobile/lib/services/user/me.dart
+++ frontend/mobile/lib/services/user/me.dart
@@ -1,9 +1,12 @@
 import 'package:area_mobile/services/dio.dart';
 import 'package:area_mobile/types/services.dart';
+import 'package:dio/dio.dart';
 
-Future<ServiceReturn<List<Service>>> getMe() async {
+Future<ServiceReturn<List<Service>>> getMe(String accessToken) async {
   try {
-    final response = await dio.get<List<dynamic>>('/me');
+    final options = Options(headers: {'Authorization': 'Bearer $accessToken'});
+    final response = await dio.get<List<dynamic>>('/me', options: options);
+
     return ServiceReturn(
         data: response.data!.map((e) => Service.fromJson(e)).toList());
   } catch (e) {
