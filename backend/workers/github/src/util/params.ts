f61318bfd0b0db620abce2774139ea738def4a6f
Florian <florian.lauch@epitech.eu>
2023-10-23 14:55:35 +0200

    fix: eslint didn't trigger prettier

--- backend/workers/github/src/util/params.ts
+++ backend/workers/github/src/util/params.ts
@@ -1,37 +1,37 @@
 import { z, ZodSchema } from "zod";
 
 export const GithubAuthSchema = z.object({
-    token_type: z.string().optional(),
-    access_token: z.string(),
-})
-type GithubAuthType = z.infer<typeof GithubAuthSchema>
+	token_type: z.string().optional(),
+	access_token: z.string(),
+});
+type GithubAuthType = z.infer<typeof GithubAuthSchema>;
 
 export default function parseArguments<T>(schema: ZodSchema): T {
-    const data: Record<string, string | GithubAuthType> = {}
-    let ret: T
+	const data: Record<string, string | GithubAuthType> = {};
+	let ret: T;
 
-    for (let i = 3; i < process.argv.length; i++) {
-        if (process.argv[i]!.startsWith("--")) {
-            const key = process.argv[i]!.substring(2)
-            const value = process.argv[i + 1]
+	for (let i = 3; i < process.argv.length; i++) {
+		if (process.argv[i]!.startsWith("--")) {
+			const key = process.argv[i]!.substring(2);
+			const value = process.argv[i + 1];
 
-            if (!value) {
-                console.error(`Error: ${process.argv[i]} requires an argument`)
-                process.exit(1)
-            }
-            data[key] = value
-        }
-    }
+			if (!value) {
+				console.error(`Error: ${process.argv[i]} requires an argument`);
+				process.exit(1);
+			}
+			data[key] = value;
+		}
+	}
 
-    try {
-        if (data.auth) {
-            const auth = GithubAuthSchema.parse(JSON.parse(data.auth as string))
-            data.auth = auth;
-        }
-        ret = schema.parse(data)
-    } catch (e) {
-        console.error(e)
-        process.exit(1)
-    }
-    return ret
+	try {
+		if (data.auth) {
+			const auth = GithubAuthSchema.parse(JSON.parse(data.auth as string));
+			data.auth = auth;
+		}
+		ret = schema.parse(data);
+	} catch (e) {
+		console.error(e);
+		process.exit(1);
+	}
+	return ret;
 }
