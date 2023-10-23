c37ca33d63af58786a6467c973c5118e665d385a
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-10-23 19:38:06 +0200

    feat(front/mobile): list Actions and Reaction from each services to display them in the hero when a service is clicked in the explore page

--- frontend/mobile/lib/components/explore/explore_hero.dart
+++ frontend/mobile/lib/components/explore/explore_hero.dart
@@ -1,3 +1,4 @@
+import 'package:area_mobile/services/services/areas.dart';
 import 'package:area_mobile/services/services/services.dart';
 import 'package:area_mobile/types/services.dart';
 import 'package:flutter/material.dart';
@@ -8,50 +9,68 @@ class ExploreHero extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Explore Services'),
-        ),
-        body: Container(
-          constraints: const BoxConstraints(maxWidth: 450, maxHeight: 800),
-          color: const Color(0xFFC5C6C6),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: FutureBuilder<ServiceReturn<List<Service>>>(
-              future: getAll(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else {
-                  final List<Service> services = [...?snapshot.data?.data];
+      appBar: AppBar(
+        title: const Text('Explore Services'),
+      ),
+      body: Container(
+        constraints: const BoxConstraints(maxWidth: 450, maxHeight: 800),
+        color: const Color(0xFFC5C6C6),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FutureBuilder<ServiceReturn<List<Service>>>(
+            future: getAll(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error: ${snapshot.error.toString()}'),
+                );
+              } else {
+                final List<Service> services = [...?snapshot.data?.data];
 
-                  return ListView.builder(
-                    itemCount: services.length,
-                    itemBuilder: (context, index) {
-                      final service = services[index];
-                      return Card(
-                        elevation: 8,
-                        child: ServiceTile(
-                          serviceName: service.id,
-                          serviceIcon: service.imageUrl,
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
-            ),
+                return ListView.builder(
+                  itemCount: services.length,
+                  itemBuilder: (context, index) {
+                    final service = services[index];
+                    return Card(
+                      elevation: 8,
+                      child: ServiceTile(
+                        serviceName: service.id,
+                        serviceIcon: service.imageUrl,
+                        onTap: () {
+                          // Open a modal or navigate to a detailed view
+                          // and pass the service ID
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  ServiceDetails(service: service),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                );
+              }
+            },
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
 class ServiceTile extends StatelessWidget {
   final String serviceName;
   final String serviceIcon;
+  final VoidCallback? onTap;
 
   const ServiceTile({
     required this.serviceName,
     required this.serviceIcon,
+    this.onTap,
     Key? key,
   }) : super(key: key);
 
@@ -64,9 +83,67 @@ class ServiceTile extends StatelessWidget {
         height: 50,
       ),
       title: Text(serviceName),
-      onTap: () {
-        // Open modal with service Properties
-      },
+      onTap: onTap,
+    );
+  }
+}
+
+class ServiceDetails extends StatefulWidget {
+  final Service service;
+
+  const ServiceDetails({Key? key, required this.service});
+
+  @override
+  _ServiceDetailsState createState() => _ServiceDetailsState();
+}
+
+class _ServiceDetailsState extends State<ServiceDetails> {
+  List<Area> actions = [];
+  List<Area> reactions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Fetch actions and reactions when the widget is initialized
+    fetchActionsAndReactions();
+  }
+
+  void fetchActionsAndReactions() async {
+    // Fetch actions and reactions for the selected service
+    final actionsResponse = await getServiceActions(widget.service.id);
+    final reactionsResponse = await getServiceReactions(widget.service.id);
+
+    if (actionsResponse.data != null) {
+      setState(() {
+        actions = actionsResponse.data!;
+      });
+    }
+
+    if (reactionsResponse.data != null) {
+      setState(() {
+        reactions = reactionsResponse.data!;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.service.id),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text('Actions:'),
+            for (var action in actions) Text(action.name),
+            const SizedBox(height: 16),
+            const Text('Reactions:'),
+            for (var reaction in reactions) Text(reaction.name),
+          ],
+        ),
+      ),
     );
   }
 }
