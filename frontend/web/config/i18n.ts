8037d3678cc918c5721562633bc8898dd9d25cc5
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-24 20:13:48 +0200

    feat(web/translations): French translations started

--- frontend/web/config/i18n.ts
+++ frontend/web/config/i18n.ts
@@ -2,11 +2,15 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import en from "@/locales/en-US";
+import fr from "@/locales/fr-FR";
 
 i18n.use(initReactI18next).init({
 	resources: {
 		en: {
 			translation: en,
+		},
+		fr: {
+			translation: fr,
 		},
 	},
 	fallbackLng: "en",
