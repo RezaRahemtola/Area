1060edb2ef28f5bbf193c9f7c8567ae2407e5727
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-10-24 15:07:03 +0200

    fix(front/mobile): remove debug print

--- frontend/mobile/lib/services/user/me.dart
+++ frontend/mobile/lib/services/user/me.dart
@@ -6,9 +6,6 @@ Future<ServiceReturn<UserMe>> getMe() async {
   try {
     final response = await dio.get<Map<String, dynamic>>('/me');
 
-    print("-------------------------------- SUCCESS");
-    print(response.data);
-    print("-------------------------------- SUCCESS");
     final responseData = response.data;
     if (responseData != null) {
       return ServiceReturn(data: UserMe.fromJson(responseData));
@@ -16,9 +13,6 @@ Future<ServiceReturn<UserMe>> getMe() async {
       return const ServiceReturn(error: "API response data is null");
     }
   } catch (e) {
-    print("-------------------------------- ERROR");
-    print(e);
-    print("-------------------------------- ERROR");
     return ServiceReturn(error: e.toString());
   }
 }
