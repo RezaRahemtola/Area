b3d02b8006f89a11260850e58adfb82e11a7faf9
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-25 22:16:53 +0200

    feat(web/settings): Icelandic translations

--- frontend/web/config/i18n.ts
+++ frontend/web/config/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 
 import en from "@/locales/en-US";
 import fr from "@/locales/fr-FR";
+import is from "@/locales/is-IS";
 
 i18n.use(initReactI18next).init({
 	resources: {
@@ -11,6 +12,9 @@ i18n.use(initReactI18next).init({
 		},
 		fr: {
 			translation: fr,
+		},
+		is: {
+			translation: is,
 		},
 	},
 	fallbackLng: "en",
