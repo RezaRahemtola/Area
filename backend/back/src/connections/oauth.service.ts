37a9c3db55bd5d996c909db7f6f6610a291ac934
Croos3r <dorian.moy@epitech.eu>
2023-10-26 00:14:30 +0200

    feat(microsoft-service-subdivision): added onenote, graph and outlook microsoft sub-services
    Took 14 seconds

--- backend/back/src/connections/oauth.service.ts
+++ backend/back/src/connections/oauth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger } from "@nestjs/common";
 import { ConnectionsService } from "./connections.service";
 import { HttpService } from "@nestjs/axios";
 import { ConfigService } from "@nestjs/config";
-import { ServiceName, ServicesService } from "../services/services.service";
+import { ServiceName, ServicesService, SubServiceNameFromServiceName } from "../services/services.service";
 import UserConnection from "./entities/user-connection.entity";
 
 type OAuthResponse = {
@@ -37,73 +37,6 @@ type ServiceOAuthFactories<TServices extends ServiceName> = {
 
 @Injectable()
 export class OauthService {
-	public readonly SERVICE_OAUTH_FACTORIES: ServiceOAuthFactories<ServiceName> = {
-		github: {
-			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
-				`${baseUrl}?client_id=${this.configService.getOrThrow("GITHUB_CLIENT_ID")}&scope=${scopes.join(
-					",",
-				)}&state=${userId}&redirect_uri=${oauthCallbackUrlFactory("github")}`,
-			connectionFactory: this.createGitHubConnection.bind(this),
-		},
-		google: {
-			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
-				`${baseUrl}?client_id=${this.configService.getOrThrow("GOOGLE_CLIENT_ID")}&scope=${encodeURI(
-					scopes.join(" "),
-				)}&access_type=offline&response_type=code&state=${userId}&prompt=consent&redirect_uri=${oauthCallbackUrlFactory(
-					"google",
-				)}`,
-			connectionFactory: this.createGoogleConnection.bind(this),
-		},
-		twitter: {
-			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
-				`${baseUrl}?client_id=${this.configService.getOrThrow("TWITTER_CLIENT_ID")}&scope=${encodeURI(
-					["offline.access", ...scopes].join(" "),
-				)}&response_type=code&state=${userId}&code_challenge=challenge&code_challenge_method=plain&redirect_uri=${oauthCallbackUrlFactory(
-					"twitter",
-				)}`,
-			connectionFactory: this.createTwitterConnection.bind(this),
-		},
-		timer: {
-			urlFactory: () => {
-				throw new Error("Cannot create OAuth URL for timer service");
-			},
-			connectionFactory: () => {
-				throw new Error("Cannot create OAuth connection for timer service");
-			},
-		},
-		linkedin: {
-			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
-				`${baseUrl}?client_id=${this.configService.getOrThrow("LINKEDIN_CLIENT_ID")}&scope=${encodeURI(
-					scopes.join(" "),
-				)}&state=${userId}&response_type=code&redirect_uri=${oauthCallbackUrlFactory("linkedin")}`,
-			connectionFactory: this.createLinkedInConnection.bind(this),
-		},
-		microsoft: {
-			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
-				`${baseUrl}?client_id=${this.configService.getOrThrow("MICROSOFT_CLIENT_ID")}&scope=${encodeURI(
-					["offline_access", ...scopes].join(" "),
-				)}&prompt=consent&response_mode=query&state=${userId}&response_type=code&redirect_uri=${oauthCallbackUrlFactory(
-					"microsoft",
-				)}`,
-			connectionFactory: this.createMicrosoftConnection.bind(this),
-		},
-		facebook: {
-			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
-				`${baseUrl}?client_id=${this.configService.getOrThrow("FACEBOOK_CLIENT_ID")}&scope=${encodeURI(
-					scopes.join(","),
-				)}&state=${userId}&response_type=${encodeURI("code granted_scopes")}&redirect_uri=${oauthCallbackUrlFactory(
-					"facebook",
-				)}`,
-			connectionFactory: this.createFacebookConnection.bind(this),
-		},
-		miro: {
-			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
-				`${baseUrl}?response_type=code&client_id=${this.configService.getOrThrow(
-					"MIRO_CLIENT_ID",
-				)}&redirect_uri=${oauthCallbackUrlFactory("miro")}&state=${userId}`,
-			connectionFactory: this.createMiroConnection.bind(this),
-		},
-	};
 	private readonly logger = new Logger(OauthService.name);
 
 	constructor(
@@ -208,7 +141,11 @@ export class OauthService {
 		return this.connectionsService.createUserConnection(userId, "linkedin", scope.split(","), connectionData);
 	}
 
-	async createMicrosoftConnection(userId: string, code: string) {
+	async createMicrosoftConnection(
+		userId: string,
+		code: string,
+		subservice: SubServiceNameFromServiceName<ServiceName, "microsoft">,
+	) {
 		const {
 			data: { scope, ...connectionData },
 		} = await this.httpService.axiosRef.post<OAuthResponse>(
@@ -218,7 +155,7 @@ export class OauthService {
 				client_secret: this.configService.getOrThrow<string>("MICROSOFT_CLIENT_SECRET"),
 				code,
 				grant_type: "authorization_code",
-				redirect_uri: this.OAUTH_CALLBACK_URL_FACTORY("microsoft"),
+				redirect_uri: this.OAUTH_CALLBACK_URL_FACTORY(`microsoft-${subservice}`),
 			},
 			{
 				headers: {
@@ -227,7 +164,12 @@ export class OauthService {
 				},
 			},
 		);
-		return this.connectionsService.createUserConnection(userId, "microsoft", scope.split(" "), connectionData);
+		return this.connectionsService.createUserConnection(
+			userId,
+			`microsoft-${subservice}`,
+			scope.split(" "),
+			connectionData,
+		);
 	}
 
 	async createFacebookConnection(userId: string, code: string, granted_scopes?: string) {
@@ -286,4 +228,83 @@ export class OauthService {
 
 	private readonly OAUTH_CALLBACK_URL_FACTORY: OAuthCallbackUrlFactory<ServiceName> = (service) =>
 		`${this.configService.getOrThrow<string>("BACK_BASE_URL")}/connections/oauth/${service}/callback`;
+
+	private readonly MICROSOFT_SUBSERVICES_OAUTH_FACTORY_FACTORY: <
+		TSubService extends SubServiceNameFromServiceName<ServiceName, "microsoft">,
+	>(
+		subService: TSubService,
+	) => {
+		urlFactory: ServiceOAuthUrlFactory<`microsoft-${TSubService}`>;
+		connectionFactory: (userId: string, code: string, granted_scopes?: string) => Promise<UserConnection>;
+	} = (subService) => ({
+		urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
+			`${baseUrl}?client_id=${this.configService.getOrThrow("MICROSOFT_CLIENT_ID")}&scope=${encodeURI(
+				["offline_access", ...scopes].join(" "),
+			)}&prompt=consent&response_mode=query&state=${userId}&response_type=code&redirect_uri=${oauthCallbackUrlFactory(
+				`microsoft-${subService}`,
+			)}`,
+		connectionFactory: (userId, code) => this.createMicrosoftConnection(userId, code, subService),
+	});
+
+	public readonly SERVICE_OAUTH_FACTORIES: ServiceOAuthFactories<ServiceName> = {
+		github: {
+			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
+				`${baseUrl}?client_id=${this.configService.getOrThrow("GITHUB_CLIENT_ID")}&scope=${scopes.join(
+					",",
+				)}&state=${userId}&redirect_uri=${oauthCallbackUrlFactory("github")}`,
+			connectionFactory: this.createGitHubConnection.bind(this),
+		},
+		google: {
+			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
+				`${baseUrl}?client_id=${this.configService.getOrThrow("GOOGLE_CLIENT_ID")}&scope=${encodeURI(
+					scopes.join(" "),
+				)}&access_type=offline&response_type=code&state=${userId}&prompt=consent&redirect_uri=${oauthCallbackUrlFactory(
+					"google",
+				)}`,
+			connectionFactory: this.createGoogleConnection.bind(this),
+		},
+		twitter: {
+			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
+				`${baseUrl}?client_id=${this.configService.getOrThrow("TWITTER_CLIENT_ID")}&scope=${encodeURI(
+					["offline.access", ...scopes].join(" "),
+				)}&response_type=code&state=${userId}&code_challenge=challenge&code_challenge_method=plain&redirect_uri=${oauthCallbackUrlFactory(
+					"twitter",
+				)}`,
+			connectionFactory: this.createTwitterConnection.bind(this),
+		},
+		timer: {
+			urlFactory: () => {
+				throw new Error("Cannot create OAuth URL for timer service");
+			},
+			connectionFactory: () => {
+				throw new Error("Cannot create OAuth connection for timer service");
+			},
+		},
+		linkedin: {
+			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
+				`${baseUrl}?client_id=${this.configService.getOrThrow("LINKEDIN_CLIENT_ID")}&scope=${encodeURI(
+					scopes.join(" "),
+				)}&state=${userId}&response_type=code&redirect_uri=${oauthCallbackUrlFactory("linkedin")}`,
+			connectionFactory: this.createLinkedInConnection.bind(this),
+		},
+		facebook: {
+			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
+				`${baseUrl}?client_id=${this.configService.getOrThrow("FACEBOOK_CLIENT_ID")}&scope=${encodeURI(
+					scopes.join(","),
+				)}&state=${userId}&response_type=${encodeURI("code granted_scopes")}&redirect_uri=${oauthCallbackUrlFactory(
+					"facebook",
+				)}`,
+			connectionFactory: this.createFacebookConnection.bind(this),
+		},
+		miro: {
+			urlFactory: (baseUrl, userId, scopes, oauthCallbackUrlFactory) =>
+				`${baseUrl}?response_type=code&client_id=${this.configService.getOrThrow(
+					"MIRO_CLIENT_ID",
+				)}&redirect_uri=${oauthCallbackUrlFactory("miro")}&state=${userId}`,
+			connectionFactory: this.createMiroConnection.bind(this),
+		},
+		"microsoft-graph": this.MICROSOFT_SUBSERVICES_OAUTH_FACTORY_FACTORY("graph"),
+		"microsoft-onenote": this.MICROSOFT_SUBSERVICES_OAUTH_FACTORY_FACTORY("onenote"),
+		"microsoft-outlook": this.MICROSOFT_SUBSERVICES_OAUTH_FACTORY_FACTORY("outlook"),
+	};
 }
