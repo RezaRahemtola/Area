715efe73059a72d9bb57771069df8f87ef125f56
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-29 14:25:19 +0100

    fix(back/workflows): Toggle taking a state (#149)

--- backend/back/src/workflows/workflows.service.ts
+++ backend/back/src/workflows/workflows.service.ts
@@ -280,21 +280,20 @@ export class WorkflowsService {
 		return affected > 0;
 	}
 
-	async toggleWorkflow(workflowId: string, ownerId: string) {
+	async toggleWorkflow(workflowId: string, newState: boolean, ownerId: string) {
 		this.logger.log(`Toggling workflow ${workflowId}...`);
 		const workflow = await this.workflowRepository.findOneBy({ id: workflowId, ownerId });
 		if (!workflow) throw new NotFoundException(`Workflow ${workflowId} not found.`);
-		const { active } = workflow;
-		await this.workflowRepository.update(workflowId, { active: !active });
-		if (active) {
+		await this.workflowRepository.update(workflowId, { active: newState });
+		if (!newState) {
 			this.logger.log(`Stopping workflow ${workflowId}...`);
 			await this.jobsService.stopWorkflowActionIfNecessary(workflowId);
 		} else {
 			this.logger.log(`Launching workflow ${workflowId}...`);
 			await this.jobsService.launchWorkflowAction(workflowId, ownerId);
 		}
-		this.logger.log(`Toggled workflow ${workflowId} to ${!active}`);
-		return { newState: !active };
+		this.logger.log(`Toggled workflow ${workflowId} to ${newState}`);
+		return { newState };
 	}
 
 	async deleteWorkflows(workflows: string[], ownerId: string) {
