9ee786acf1a03367e3db756c2fa685d93efd79fc
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-30 13:28:38 +0100

    feat(mobile/editor): Basic cards design

--- frontend/mobile/lib/pages/editor.dart
+++ frontend/mobile/lib/pages/editor.dart
@@ -1,3 +1,4 @@
+import 'package:area_mobile/colors.dart';
 import 'package:area_mobile/types/workflows/workflows.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -78,6 +79,42 @@ class _EditorState extends State<Editor> {
                       });
                     },
                   )),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    // TODO: update with service logo if defined
+                    leading: Image.asset(
+                      'assets/bolt.png',
+                      color: primaryColor,
+                    ),
+                    title: Text(AppLocalizations.of(context)!.action),
+                    subtitle: Text(
+                        AppLocalizations.of(context)!.editorActionDescription),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    // TODO: update with service logo if defined
+                    leading: Image.asset(
+                      'assets/bolt.png',
+                      color: primaryColor,
+                    ),
+                    title: Text(AppLocalizations.of(context)!.reaction),
+                    subtitle: Text(AppLocalizations.of(context)!
+                        .editorReactionDescription),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
