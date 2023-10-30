37572d9f892b14d0f99443791f4fa21d0cd7e0a5
Florian <florian.lauch@epitech.eu>
2023-10-30 15:56:04 +0100

    feat(back): Gitlab push & pull request actions

--- backend/back/src/webhooks/webhooks.module.ts
+++ backend/back/src/webhooks/webhooks.module.ts
@@ -3,10 +3,11 @@ import { WebhooksController } from "./webhooks.controller";
 import { GithubWebhookService } from "./services/gihub-webhook.service";
 import { GrpcModule } from "../grpc/grpc.module";
 import { LinearWebhookService } from "./services/linear-webhook.service";
+import { GitlabWebhookService } from "./services/gitlab-webhook.service";
 
 @Module({
 	imports: [GrpcModule],
 	controllers: [WebhooksController],
-	providers: [GithubWebhookService, LinearWebhookService],
+	providers: [GithubWebhookService, GitlabWebhookService, LinearWebhookService],
 })
 export class WebhooksModule {}
