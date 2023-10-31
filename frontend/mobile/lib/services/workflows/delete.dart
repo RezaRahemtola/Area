7a2495847529bd0600df93dc2b3fc97e587bc569
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-10-31 18:37:07 +0100

    feat(mobile/library): Closing the popup menu on action and refreshing data

--- frontend/mobile/lib/services/workflows/delete.dart
+++ frontend/mobile/lib/services/workflows/delete.dart
@@ -5,12 +5,8 @@ import 'package:area_mobile/types/workflows/workflows.dart';
 Future<ServiceReturn<int>> delete(Workflow old) async {
   try {
     final response = await dio.delete<Workflow>('/workflows/${old.id}');
-    print("status code is :");
-    print(response.statusCode);
     return ServiceReturn(data: response.statusCode);
   } catch (e) {
-    print("error is :");
-    print(e.toString());
     return ServiceReturn(error: e.toString());
   }
 }
--- frontend/mobile/lib/services/workflows/patch.dart
+++ frontend/mobile/lib/services/workflows/patch.dart
@@ -1,23 +1,14 @@
 import 'package:area_mobile/services/dio.dart';
 import 'package:area_mobile/types/services.dart';
 import 'package:area_mobile/types/workflows/workflows.dart';
-import 'package:dio/dio.dart';
 
 Future<ServiceReturn<int>> rename(Workflow old, String newName) async {
-  print("name is");
-  print(newName);
   try {
-    final response = await dio.patch<Workflow>('/workflows/${old.id}',
-        data: {
-          "name": newName,
-        },
-        options: Options(contentType: Headers.jsonContentType));
-    print("status code is :");
-    print(response.statusCode);
+    final response = await dio.patch<Workflow>('/workflows/${old.id}', data: {
+      "name": newName,
+    });
     return ServiceReturn(data: response.statusCode);
   } catch (e) {
-    print("error is :");
-    print(e.toString());
     return ServiceReturn(error: e.toString());
   }
 }
