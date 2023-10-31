6b623124d8d89ef9e3691c91e2c353b2a67208a9
Axel Denis <axel.denis@epitech.eu>
2023-10-31 17:32:57 +0100

    feat(delete): the lib menu can now delete a workflow)

--- frontend/mobile/lib/services/workflows/index.dart
+++ frontend/mobile/lib/services/workflows/index.dart
@@ -1,5 +1,6 @@
 import 'package:area_mobile/services/workflows/get.dart';
 import 'package:area_mobile/services/workflows/patch.dart';
+import 'package:area_mobile/services/workflows/delete.dart';
 import 'package:area_mobile/types/services.dart';
 import 'package:area_mobile/types/workflows/workflows.dart';
 
@@ -8,10 +9,14 @@ class WorkflowsService {
   final Future<ServiceReturn<Workflow>> Function(String serviceId) getOne;
   final Future<ServiceReturn<int>> Function(Workflow old, String newName)
       rename;
+  final Future<ServiceReturn<int>> Function(Workflow old) delete;
 
   const WorkflowsService(
-      {required this.getAll, required this.getOne, required this.rename});
+      {required this.getAll,
+      required this.getOne,
+      required this.rename,
+      required this.delete});
 }
 
-const workflowsService =
-    WorkflowsService(getAll: getAll, getOne: getOne, rename: rename);
+const workflowsService = WorkflowsService(
+    getAll: getAll, getOne: getOne, rename: rename, delete: delete);
