42fb8ab9c23a95fe50e31920528c266eec6131fd
Florian <florian.lauch@epitech.eu>
2023-10-31 23:04:57 +0100

    fix: copy proto shell

--- scripts/copy_proto.sh
+++ scripts/copy_proto.sh
@@ -7,23 +7,25 @@ if ! [[ -d "$PROTO_PATH" ]]; then
   echo Protobuf files not found! Cloning...
   git clone git@github.com:RezaRahemtola/area-proto.git $PROTO_PATH
   find $PROTO_PATH -not -name "*proto" -exec rm -rf {} \;
-else
-  git -C $PROTO_PATH pull
 fi
 
 echo Copying protobuf files...
 
 rm -rf $BACK_PROTO_PATH $SUPERVISOR_PROTO_PATH
-rm -rf $GIT_ROOT_PATH/backend/workers/seconds-interval/proto
-rm -rf $GIT_ROOT_PATH/backend/workers/google/proto
-rm -rf $GIT_ROOT_PATH/backend/workers/linkedin/proto
 rm -rf $GIT_ROOT_PATH/backend/workers/github/proto
+rm -rf $GIT_ROOT_PATH/backend/workers/gitlab/proto
+rm -rf $GIT_ROOT_PATH/backend/workers/google/proto
+rm -rf $GIT_ROOT_PATH/backend/workers/linear/proto
+rm -rf $GIT_ROOT_PATH/backend/workers/linkedin/proto
+rm -rf $GIT_ROOT_PATH/backend/workers/seconds-interval/proto
 
 cp -rf $PROTO_PATH $BACK_PROTO_PATH
 cp -rf $PROTO_PATH $SUPERVISOR_PROTO_PATH
-cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/seconds-interval/proto
-cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/google/proto
-cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/linkedin/proto
 cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/github/proto
+cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/gitlab/proto
+cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/google/proto
+cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/linear/proto
+cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/linkedin/proto
+cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/seconds-interval/proto
 
 echo Protobuf files copied.
