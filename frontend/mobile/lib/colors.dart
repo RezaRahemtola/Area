bb018cefef6ea0ce94813b089cc099c20f52028f
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-11-01 17:30:32 +0100

    fix(front/mobile): repair and setup theme for light and dark mode with a themeMode to switch between

--- frontend/mobile/lib/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color(0xFF516079);
-const secondaryColor = Color(0xFFF1895C);
-const accentColor = Color(0xFFC5C6C6);
-const neutralColor = Color(0xFFFFFFFF);
