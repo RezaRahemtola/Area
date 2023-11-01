f1a89b50d07844c35978e65b21fc9d431a14726a
Axel Denis <axel.denis@epitech.eu>
2023-11-01 23:05:25 +0100

    feat(lang): languages are applied to the empty notice

--- frontend/mobile/lib/components/empty_notice.dart
+++ frontend/mobile/lib/components/empty_notice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class EmptyNotice extends StatelessWidget {
   const EmptyNotice({
@@ -8,13 +9,15 @@ class EmptyNotice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 25.0),
-      padding: const EdgeInsets.only(left: 75.0, right: 75.0, top: 15.0, bottom: 15.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: const BorderRadius.all(Radius.circular(15.0)),
-      ),
-      child: const Text("nothing to show here"),
-    );
+        margin: const EdgeInsets.only(top: 25.0),
+        padding: const EdgeInsets.only(
+            left: 75.0, right: 75.0, top: 15.0, bottom: 15.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+        ),
+        child: Text(
+          AppLocalizations.of(context)!.emptyNotice,
+        ));
   }
 }
