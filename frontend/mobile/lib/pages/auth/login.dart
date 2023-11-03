8d6ad4e9e23240296b087dd2c848ec9a6533a760
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-11-03 20:29:36 +0100

    feat(mobile/auth): Complete login and register OAuth

--- frontend/mobile/lib/pages/auth/login.dart
+++ frontend/mobile/lib/pages/auth/login.dart
@@ -3,9 +3,11 @@ import 'package:area_mobile/components/auth/password_field.dart';
 import 'package:area_mobile/pages/auth/register.dart';
 import 'package:area_mobile/services/dio.dart';
 import 'package:area_mobile/storage/index.dart';
+import 'package:area_mobile/types/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class LoginPage extends StatefulWidget {
@@ -27,6 +29,7 @@ class _LoginPageState extends State<LoginPage> {
   WebViewController? webviewController;
   bool authInProgress = false;
   String? oauthUrl;
+  bool isLogin = false;
 
   void _onOAuthLogin(String url) {
     setState(() {
@@ -57,71 +60,93 @@ class _LoginPageState extends State<LoginPage> {
     });
   }
 
+  void _onAuthSwitch(bool newIsLogin) {
+    setState(() {
+      isLogin = newIsLogin;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return StatefulBuilder(
         builder: (BuildContext context, StateSetter setModalState) {
       return authInProgress
           ? WebViewWidget(controller: webviewController!)
-          : Scaffold(
-              body: Container(
-                  constraints:
-                      const BoxConstraints(maxWidth: 450, maxHeight: 1000),
-                  color: const Color(0xFF516079),
-                  child: Form(
-                      key: _formKey,
-                      child: Center(
-                          child: Card(
-                              elevation: 8,
-                              child: Container(
-                                constraints: const BoxConstraints(
-                                    maxWidth: 400, maxHeight: 600),
-                                padding: const EdgeInsets.all(32.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    const Image(
+          : isLogin
+              ? Scaffold(
+                  body: Container(
+                      constraints:
+                          const BoxConstraints(maxWidth: 450, maxHeight: 1000),
+                      color: const Color(0xFF516079),
+                      child: Form(
+                          key: _formKey,
+                          child: Center(
+                              child: Card(
+                                  elevation: 8,
+                                  child: Container(
+                                    constraints: const BoxConstraints(
+                                        maxWidth: 400, maxHeight: 600),
+                                    padding: const EdgeInsets.all(32.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        const Image(
                                 image: AssetImage('assets/logo.png'),
                                 height: 150,
                                 width: 150,
                               ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(16.0),
-                                      child: Text(
-                                        AppLocalizations.of(context)!.welcome,
-                                        textAlign: TextAlign.center,
-                                        style:
-                                      Theme.of(context).textTheme.headlineSmall,
-                                      ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: Text(
+                                            AppLocalizations.of(context)!
+                                                .welcome,
+                                            textAlign: TextAlign.center,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headlineSmall,
+                                          ),
+                                        ),
+                                        EmailField(
+                                            emailController: emailController),
+                                        PasswordField(
+                                          passwordController:
+                                              passwordController,
+                                          label: AppLocalizations.of(context)!
+                                              .password,
+                                        ),
+                                        LoginButtons(
+                                          formKey: _formKey,
+                                          emailController: emailController,
+                                          passwordController:
+                                              passwordController,
+                                          onSuccess: widget.onSuccess,
+                                          onOAuthLogin: _onOAuthLogin,
+                                          onAuthSwitch: _onAuthSwitch,
+                                          updateTheme: widget.updateTheme
+                                        )
+                                      ],
                                     ),
-                                    EmailField(
-                                        emailController: emailController),
-                                    PasswordField(
-                                      passwordController: passwordController,
-                                      label: AppLocalizations.of(context)!
-                                          .password,
-                                    ),
-                                    LoginButtons(
-                                        formKey: _formKey,
-                                        emailController: emailController,
-                                        passwordController: passwordController,
-                                        onSuccess: widget.onSuccess,
-                                        onOAuthLogin: _onOAuthLogin,
-                                    updateTheme: widget.updateTheme)
-                                  ],
-                                ),
-                              ))))));
+                                  ))))))
+              : RegisterPage(
+                  onSuccess: widget.onSuccess,
+                  onAuthSwitch: _onAuthSwitch,
+                  onOAuthRegister: _onOAuthLogin,
+        updateTheme: widget.updateTheme
+                );
     });
   }
 }
 
-class LoginButtons extends StatefulWidget {
+class LoginButtons extends StatelessWidget {
   final GlobalKey<FormState> _formKey;
   final TextEditingController emailController;
   final TextEditingController passwordController;
   final Function() onSuccess;
   final Function(String url) onOAuthLogin;
+  final Function(bool isRegister) onAuthSwitch;
   final Function(String theme) updateTheme;
 
   const LoginButtons(
@@ -131,15 +156,9 @@ class LoginButtons extends StatefulWidget {
       required this.passwordController,
       required this.onSuccess,
       required this.onOAuthLogin,
+      required this.onAuthSwitch,
       required this.updateTheme})
       : _formKey = formKey;
-
-  @override
-  State<LoginButtons> createState() => _LoginButtonsState();
-}
-
-class _LoginButtonsState extends State<LoginButtons> {
-  WebViewController? webviewController;
 
   @override
   Widget build(BuildContext context) {
@@ -152,10 +171,7 @@ class _LoginButtonsState extends State<LoginButtons> {
           children: [
             ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => RegisterPage(updateTheme: updateTheme,)));
+                  onAuthSwitch(false);
                 },
                 child: Row(
                   children: [
@@ -168,15 +184,14 @@ class _LoginButtonsState extends State<LoginButtons> {
                 )),
             ElevatedButton(
                 onPressed: () async {
-                  if (widget._formKey.currentState!.validate()) {
-                    final result = await services.auth.login(
-                        widget.emailController.text,
-                        widget.passwordController.text);
+                  if (_formKey.currentState!.validate()) {
+                    final result = await services.auth
+                        .login(emailController.text, passwordController.text);
                     if (!context.mounted) {
                       return;
                     }
                     if (result.data != null) {
-                      widget.onSuccess();
+                      onSuccess();
                     } else if (result.error != null) {
                       ScaffoldMessenger.of(context).showSnackBar(
                         SnackBar(content: Text(result.error!)),
@@ -187,35 +202,43 @@ class _LoginButtonsState extends State<LoginButtons> {
                 child: Text(AppLocalizations.of(context)!.login)),
           ],
         ),
-        Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  final result =
-                      await services.connections.authenticate("github");
-                  widget.onOAuthLogin(result.data!);
-                },
-                child: const Text("GitHub"),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  final result =
-                      await services.connections.authenticate("google");
-                  widget.onOAuthLogin(result.data!);
-                },
-                child: const Text("Google"),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  final result =
-                      await services.connections.authenticate("microsoft");
-                  widget.onOAuthLogin(result.data!);
-                },
-                child: const Text("Microsoft"),
-              )
-            ])
+        FutureBuilder<ServiceReturn<List<Service>>>(
+          future: services.services.getAll(isAuthenticator: true),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(AppLocalizations.of(context)!
+                    .error(snapshot.error.toString())),
+              );
+            } else {
+              final List<Service> oAuthServices = [...?snapshot.data?.data];
+
+              return Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ...oAuthServices
+                        .map(
+                          (oAuthService) => IconButton(
+                            icon: SvgPicture.network(
+                              oAuthService.imageUrl,
+                              height: 64,
+                              width: 64,
+                            ),
+                            onPressed: () async {
+                              final result = await services.connections
+                                  .authenticate(oAuthService.id);
+                              onOAuthLogin(result.data!);
+                            },
+                          ),
+                        )
+                        .toList()
+                  ]);
+            }
+          },
+        ),
       ]);
     });
   }
--- frontend/mobile/lib/services/services/index.dart
+++ frontend/mobile/lib/services/services/index.dart
@@ -3,7 +3,8 @@ import 'package:area_mobile/services/services/services.dart';
 import 'package:area_mobile/types/services.dart';
 
 class ServicesService {
-  final Future<ServiceReturn<List<Service>>> Function([String has]) getAll;
+  final Future<ServiceReturn<List<Service>>> Function(
+      {String? has, bool isAuthenticator}) getAll;
   final Future<ServiceReturn<Service>> Function(String serviceId) getOne;
 
   final Future<ServiceReturn<List<Area>>> Function(String serviceId)
