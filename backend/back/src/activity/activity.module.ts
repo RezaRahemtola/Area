feb02611d5384b7a2f4561fd2230215b0e72b451
Croos3r <dorian.moy@epitech.eu>
2023-11-03 06:15:47 +0100

    feat(jobs-service): made activity logging work and produce logs by hooking on actions/reactions events
    Took 53 seconds

--- backend/back/src/activity/activity.module.ts
+++ backend/back/src/activity/activity.module.ts
@@ -3,9 +3,10 @@ import { ActivityService } from "./activity.service";
 import { ActivityController } from "./activity.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import ActivityLog from "./entities/activity-log.entity";
+import WorkflowArea from "../workflows/entities/workflow-area.entity";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([ActivityLog])],
+	imports: [TypeOrmModule.forFeature([ActivityLog, WorkflowArea])],
 	controllers: [ActivityController],
 	providers: [ActivityService],
 	exports: [ActivityService],
