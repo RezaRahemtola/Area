abd92c3cbf904e3e05f4685cdb4701b8c4983849
Axel Denis <91873203+axel-denis@users.noreply.github.com>
2023-11-03 22:15:27 +0100

    feat(mobile/library): Search bar (#172)
    * feat(search): the searchbar is finally working git add frontend/git add frontend/
    
    * typo: made dart format for the review
    
    * typo: corrected unused variable
    
    * typo: corrected unused variable
    
    * feat(lang): added the search bar to the language files
    
    * fix&feat(library): No more crashes when no workflows are available & searchBar is now traducted
    
    * improv(lib): accepted Reza change request
    
    Co-authored-by: Reza Rahemtola <reza.rahemtola@epitech.eu>
    
    * improv(library): made Reza's requested changes
    
    * fix(merge): correcting the merge mess
    
    * fix(mobile/library): Search bar
    
    * fix(mobile/library): Seach bar dark theme
    
    ---------
    
    Co-authored-by: Reza Rahemtola <reza.rahemtola@epitech.eu>

--- frontend/mobile/lib/pages/library.dart
+++ frontend/mobile/lib/pages/library.dart
@@ -14,10 +14,23 @@ class Library extends StatefulWidget {
 
 class _LibraryState extends State<Library> {
   List<Workflow> workflows = [];
+  bool needRefresh = true;
+  List<Workflow> initialWorkflows = [];
+
+  final TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+  }
+
+  void _updateSearch(String search) {
+    setState(() {
+      workflows = initialWorkflows
+          .where((element) =>
+              element.name.toLowerCase().contains(search.toLowerCase()))
+          .toList();
+    });
   }
 
   @override
@@ -32,34 +45,75 @@ class _LibraryState extends State<Library> {
         color: Theme.of(context).colorScheme.onSecondary,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: FutureBuilder<ServiceReturn<List<Workflow>>>(
-            future: services.workflows.getAll(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text(AppLocalizations.of(context)!
-                      .error(snapshot.error.toString())),
-                );
-              } else {
-                workflows = snapshot.data!.data!;
-                return ListView.builder(
-                  itemCount: workflows.length,
-                  itemBuilder: (context, index) {
-                    return WorkflowTile(
-                      workflow: workflows[index],
-                      onUpdate: () async {
-                        final newWorkflows = await services.workflows.getAll();
-                        setState(() {
-                          workflows = newWorkflows.data!;
-                        });
-                      },
-                    );
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Container(
+                  color: Theme.of(context).colorScheme.onSecondary,
+                  child: TextField(
+                    controller: searchController,
+                    onEditingComplete: () =>
+                        _updateSearch(searchController.text),
+                    decoration: InputDecoration(
+                      hintText: AppLocalizations.of(context)!.searchBar,
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.clear),
+                        onPressed: () => searchController.clear(),
+                      ),
+                      prefixIcon: IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: () {
+                          _updateSearch(searchController.text);
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FutureBuilder<ServiceReturn<List<Workflow>>>(
+                  future: services.workflows.getAll(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text(AppLocalizations.of(context)!
+                            .error(snapshot.error.toString())),
+                      );
+                    } else {
+                      if (needRefresh) {
+                        needRefresh = false;
+                        if (snapshot.data!.data != null) {
+                          initialWorkflows = snapshot.data!.data!;
+                          workflows = snapshot.data!.data!;
+                        }
+                      }
+                      return ListView.builder(
+                        itemCount: workflows.length,
+                        itemBuilder: (context, index) {
+                          return WorkflowTile(
+                            workflow: workflows[index],
+                            onUpdate: () async {
+                              final newWorkflows =
+                                  await services.workflows.getAll();
+                              setState(() {
+                                initialWorkflows = newWorkflows.data!;
+                              });
+                              _updateSearch(searchController.text);
+                            },
+                          );
+                        },
+                      );
+                    }
                   },
-                );
-              }
-            },
+                ),
+              ),
+            ],
           ),
         ),
       ),
