fddac6ce546b28fd060d8d00717a9a0c347165b5
Florian <florian.lauch@epitech.eu>
2023-11-05 02:14:18 +0100

    fix: migration and early return

--- backend/back/src/workflows/seed/1699145276143-CreateAreaOnActionAction.ts
+++ backend/back/src/workflows/seed/1699145276143-CreateAreaOnActionAction.ts
@@ -4,8 +4,7 @@ export class CreateAreaOnActionAction1699145276143 implements MigrationInterface
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
 			`INSERT INTO "area" ("id", "service_id", "is_action", "description", "parameters_form_flow", "parameters_return_flow")
-            VALUES ('on-action', 'area', true, 'Triggers when a workflow''s action is triggered', $1, '{}')`,
-			[JSON.stringify([])],
+            VALUES ('on-action', 'area', true, 'Triggers when a workflow''s action is triggered', '[]', '{}')`,
 		);
 	}
 
