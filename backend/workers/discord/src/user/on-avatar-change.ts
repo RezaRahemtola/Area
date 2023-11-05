102217b253d838a76f32ec15b89e0e9ae9e36b78
Croos3r <dorian.moy@epitech.eu>
2023-11-05 06:56:41 +0100

    fix(discord-worker-on-avatar-change): removed useless guildId snowflake param
    Took 5 minutes

--- backend/workers/discord/src/user/on-avatar-change.ts
+++ backend/workers/discord/src/user/on-avatar-change.ts
@@ -1,21 +1,14 @@
 import DiscordOauth2 from "discord-oauth2";
 import { credentials } from "@grpc/grpc-js";
-import { z } from "zod";
 import parseArguments from "../util/params";
 import { AreaBackServiceClient } from "../proto/area_back";
-import { DiscordDataSchema, DiscordSnowFlakeSchema } from "../util/types";
+import { DiscordDataSchema } from "../util/types";
 import { onAction } from "../util/grpc";
 
 const REFRESH_TIMEOUT_SECONDS = 10;
 
-const OnAvatarChangeParamsSchema = z.object({
-	guildId: DiscordSnowFlakeSchema,
-});
-
-const OnAvatarChangeSchema = z.intersection(OnAvatarChangeParamsSchema, DiscordDataSchema);
-
 export default async function onAvatarChange(discordClient: DiscordOauth2) {
-	const params = parseArguments(OnAvatarChangeSchema);
+	const params = parseArguments(DiscordDataSchema);
 	const client = new AreaBackServiceClient(params.target ?? "localhost:50050", credentials.createInsecure());
 	const getUser = async () => discordClient.getUser(params.auth.access_token);
 	let { avatar: initialAvatarHash } = await getUser();
