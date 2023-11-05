cc63b5bc2778103bb017de7749326ad86f0f46e3
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-11-05 17:48:15 +0100

    feat(web/landing): Pricing page

--- frontend/web/app/(landing)/pricing/page.tsx
+++ frontend/web/app/(landing)/pricing/page.tsx
@@ -1,30 +1,25 @@
 import PricingPlanCard from "@/components/landing/pricing/PricingPlanCard";
 
 const PricingPage = () => (
-	<section className="min-h-screen w-full py-12 bg-accent flex items-center justify-center">
-		<div className="container px-4 md:px-6">
-			<div className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-3 md:gap-8">
+	<section className="min-h-screen w-full py-12 bg-accent">
+		<h1 className="text-5xl font-extrabold text-center mb-10">Pricing</h1>
+		<div className="container px-4 md:px-6 flex min-w-full justify-center">
+			<div className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-3 md:gap-8 mt-10">
 				<PricingPlanCard
 					name="Basic"
 					price={29}
-					features={["720p Video Rendering", "2GB Cloud Storage", "Basic Video Templates"]}
+					features={["5 workflows", "Workflows Templates", "Access all Services"]}
 				/>
 				<PricingPlanCard
 					name="Pro"
 					price={59}
-					features={["1080p Video Rendering", "10GB Cloud Storage", "Premium Video Templates", "Collaboration Tools"]}
+					features={["100 workflows", "Pass Params between Areas", "Multi-reactions Workflows", "Custom Activity Logs"]}
 					isPopular
 				/>
 				<PricingPlanCard
 					name="Enterprise"
 					price={99}
-					features={[
-						"4K Video Rendering",
-						"Unlimited Cloud Storage",
-						"Custom Video Templates",
-						"Advanced Collaboration Tools",
-						"Dedicated Support",
-					]}
+					features={["Unlimited workflows", "Premium Services", "Dedicated Support", "On-site account setup"]}
 				/>
 			</div>
 		</div>
