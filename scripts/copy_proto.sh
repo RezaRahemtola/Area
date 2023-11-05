44f1479d9d689d335f6963921b2fd109e2a756d0
Croos3r <dorian.moy@epitech.eu>
2023-11-05 02:16:40 +0100

    fix(discord-worker): re-ordered copy_proto.sh rm and cp to have discord in alpha order gngngngng @EdenComp
    Took 33 seconds

--- scripts/copy_proto.sh
+++ scripts/copy_proto.sh
@@ -13,6 +13,7 @@ echo Copying protobuf files...
 
 rm -rf $BACK_PROTO_PATH $SUPERVISOR_PROTO_PATH
 rm -rf $GIT_ROOT_PATH/backend/workers/airtable/proto
+rm -rf $GIT_ROOT_PATH/backend/workers/discord/proto
 rm -rf $GIT_ROOT_PATH/backend/workers/github/proto
 rm -rf $GIT_ROOT_PATH/backend/workers/gitlab/proto
 rm -rf $GIT_ROOT_PATH/backend/workers/google/proto
@@ -22,11 +23,11 @@ rm -rf $GIT_ROOT_PATH/backend/workers/microsoft/proto
 rm -rf $GIT_ROOT_PATH/backend/workers/seconds-interval/proto
 rm -rf $GIT_ROOT_PATH/backend/workers/todoist/proto
 rm -rf $GIT_ROOT_PATH/backend/workers/twitter/proto
-rm -rf $GIT_ROOT_PATH/backend/workers/discord/proto
 
 cp -rf $PROTO_PATH $BACK_PROTO_PATH
 cp -rf $PROTO_PATH $SUPERVISOR_PROTO_PATH
 cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/airtable/proto
+cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/discord/proto
 cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/github/proto
 cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/gitlab/proto
 cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/google/proto
@@ -36,6 +37,5 @@ cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/microsoft/proto
 cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/seconds-interval/proto
 cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/todoist/proto
 cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/twitter/proto
-cp -rf $PROTO_PATH $GIT_ROOT_PATH/backend/workers/discord/proto
 
 echo Protobuf files copied.
